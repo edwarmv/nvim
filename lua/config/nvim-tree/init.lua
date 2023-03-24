0b52c480b86bf0a9eef28ad7500ada6b20f2615e
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2023-03-24 16:50:07 -0400

    feat: use nvim-tree

--- lua/config/nvim-tree/init.lua
+++ lua/config/nvim-tree/init.lua
@@ -1,4 +1,7 @@
 require("config.nvim-tree.custom")
+local M = {}
+
+local api = require("nvim-tree.api")
 
 local icons = require("icons")
 
@@ -19,98 +22,19 @@ end
 -- end
 
 require("nvim-tree").setup({
-  auto_reload_on_write = true,
-  create_in_closed_folder = false,
-  sync_root_with_cwd = true,
-  disable_netrw = false,
-  hijack_cursor = false,
-  hijack_netrw = true,
-  hijack_unnamed_buffer_when_opening = false,
-  ignore_buffer_on_setup = false,
-  open_on_setup = false,
-  open_on_setup_file = false,
-  open_on_tab = false,
-  sort_by = "name",
-  update_cwd = false,
-  reload_on_bufenter = false,
-  respect_buf_cwd = false,
   view = {
     width = {
       min = 30,
       max = -1,
     },
-    hide_root_folder = false,
-    side = "left",
     preserve_window_proportions = true,
     number = false,
     relativenumber = false,
     signcolumn = "no",
-    mappings = {
-      custom_only = true,
-      list = {
-        { key = { "<CR>", "o", "<2-LeftMouse>" }, action = "edit" },
-        { key = "<C-e>", action = "edit_in_place" },
-        { key = "O", action = "edit_no_picker" },
-        { key = { "}", "<2-RightMouse>" }, action = "cd" },
-        { key = "<C-v>", action = "vsplit" },
-        { key = "<C-s>", action = "split" },
-        { key = "<C-t>", action = "tabnew" },
-        { key = "<", action = "prev_sibling" },
-        { key = ">", action = "next_sibling" },
-        { key = "P", action = "parent_node" },
-        { key = "<BS>", action = "close_node" },
-        { key = "<Tab>", action = "preview" },
-        { key = "<C-k>", action = "first_sibling" },
-        { key = "<C-j>", action = "last_sibling" },
-        { key = "I", action = "toggle_git_ignored" },
-        { key = "H", action = "toggle_dotfiles" },
-        { key = "U", action = "toggle_custom" },
-        { key = "R", action = "refresh" },
-        { key = "a", action = "create" },
-        { key = "d", action = "remove" },
-        { key = "D", action = "trash" },
-        { key = "r", action = "rename" },
-        { key = "<C-r>", action = "full_rename" },
-        { key = "x", action = "cut" },
-        { key = "c", action = "copy" },
-        { key = "p", action = "paste" },
-        { key = "y", action = "copy_name" },
-        { key = "Y", action = "copy_path" },
-        { key = "gy", action = "copy_absolute_path" },
-        { key = "[e", action = "prev_diag_item" },
-        { key = "[c", action = "prev_git_item" },
-        { key = "]e", action = "next_diag_item" },
-        { key = "]c", action = "next_git_item" },
-        { key = "{", action = "dir_up" },
-        { key = "s", action = "system_open" },
-        { key = "f", action = "live_filter" },
-        { key = "F", action = "clear_live_filter" },
-        { key = "q", action = "close" },
-        { key = "W", action = "collapse_all" },
-        { key = "E", action = "expand_all" },
-        { key = "S", action = "search_node" },
-        { key = ".", action = "run_file_command" },
-        { key = "K", action = "toggle_file_info" },
-        { key = "g?", action = "toggle_help" },
-        -- { key = "m", action = "toggle_mark" },
-        -- { key = "bmv", action = "bulk_move" },
-      },
-    },
   },
   renderer = {
-    add_trailing = false,
-    group_empty = false,
-    highlight_git = false,
-    highlight_opened_files = "no",
-    root_folder_modifier = ":~",
     indent_markers = {
       enable = true,
-      icons = {
-        corner = "└",
-        edge = "│",
-        item = "│",
-        none = " ",
-      },
     },
     icons = {
       webdev_colors = true,
@@ -150,39 +74,71 @@ require("nvim-tree").setup({
     },
     special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
   },
-  hijack_directories = {
-    enable = false,
-    auto_open = true,
-  },
-  update_focused_file = {
-    enable = false,
-    update_cwd = false,
-    ignore_list = {},
-  },
-  ignore_ft_on_setup = {},
-  system_open = {
-    cmd = "",
-    args = {},
-  },
+  on_attach = function(bufnr)
+    local function opts(desc)
+      return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+    vim.keymap.set("n", "L", api.tree.change_root_to_node, opts("CD"))
+    vim.keymap.set("n", "<C-e>", api.node.open.replace_tree_buffer, opts("Open: In Place"))
+    vim.keymap.set("n", "<C-k>", api.node.show_info_popup, opts("Info"))
+    vim.keymap.set("n", "<C-r>", api.fs.rename_sub, opts("Rename: Omit Filename"))
+    vim.keymap.set("n", "<C-t>", api.node.open.tab, opts("Open: New Tab"))
+    vim.keymap.set("n", "<C-v>", api.node.open.vertical, opts("Open: Vertical Split"))
+    vim.keymap.set("n", "<C-s>", api.node.open.horizontal, opts("Open: Horizontal Split"))
+    vim.keymap.set("n", "<BS>", api.node.navigate.parent_close, opts("Close Directory"))
+    vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
+    vim.keymap.set("n", "<Tab>", api.node.open.preview, opts("Open Preview"))
+    vim.keymap.set("n", ">", api.node.navigate.sibling.next, opts("Next Sibling"))
+    vim.keymap.set("n", "<", api.node.navigate.sibling.prev, opts("Previous Sibling"))
+    vim.keymap.set("n", "<m-s>", api.node.run.cmd, opts("Run Command"))
+    vim.keymap.set("n", "H", api.tree.change_root_to_parent, opts("Up"))
+    vim.keymap.set("n", "a", api.fs.create, opts("Create"))
+    vim.keymap.set("n", "bmv", api.marks.bulk.move, opts("Move Bookmarked"))
+    vim.keymap.set("n", "B", api.tree.toggle_no_buffer_filter, opts("Toggle No Buffer"))
+    vim.keymap.set("n", "c", api.fs.copy.node, opts("Copy"))
+    vim.keymap.set("n", "C", api.tree.toggle_git_clean_filter, opts("Toggle Git Clean"))
+    vim.keymap.set("n", "[c", api.node.navigate.git.prev, opts("Prev Git"))
+    vim.keymap.set("n", "]c", api.node.navigate.git.next, opts("Next Git"))
+    vim.keymap.set("n", "d", api.fs.remove, opts("Delete"))
+    vim.keymap.set("n", "D", api.fs.trash, opts("Trash"))
+    vim.keymap.set("n", "E", api.tree.expand_all, opts("Expand All"))
+    vim.keymap.set("n", "e", api.fs.rename_basename, opts("Rename: Basename"))
+    vim.keymap.set("n", "]e", api.node.navigate.diagnostics.next, opts("Next Diagnostic"))
+    vim.keymap.set("n", "[e", api.node.navigate.diagnostics.prev, opts("Prev Diagnostic"))
+    vim.keymap.set("n", "F", api.live_filter.clear, opts("Clean Filter"))
+    vim.keymap.set("n", "f", api.live_filter.start, opts("Filter"))
+    vim.keymap.set("n", "g?", api.tree.toggle_help, opts("Help"))
+    vim.keymap.set("n", "gy", api.fs.copy.absolute_path, opts("Copy Absolute Path"))
+    vim.keymap.set("n", ".", api.tree.toggle_hidden_filter, opts("Toggle Dotfiles"))
+    vim.keymap.set("n", "I", api.tree.toggle_gitignore_filter, opts("Toggle Git Ignore"))
+    vim.keymap.set("n", "J", api.node.navigate.sibling.last, opts("Last Sibling"))
+    vim.keymap.set("n", "K", api.node.navigate.sibling.first, opts("First Sibling"))
+    vim.keymap.set("n", "m", api.marks.toggle, opts("Toggle Bookmark"))
+    vim.keymap.set("n", "o", api.node.open.edit, opts("Open"))
+    vim.keymap.set("n", "O", api.node.open.no_window_picker, opts("Open: No Window Picker"))
+    vim.keymap.set("n", "p", api.fs.paste, opts("Paste"))
+    vim.keymap.set("n", "P", api.node.navigate.parent, opts("Parent Directory"))
+    vim.keymap.set("n", "q", api.tree.close, opts("Close"))
+    vim.keymap.set("n", "r", api.fs.rename, opts("Rename"))
+    vim.keymap.set("n", "R", api.tree.reload, opts("Refresh"))
+    vim.keymap.set("n", "s", api.node.run.system, opts("Run System"))
+    vim.keymap.set("n", "S", api.tree.search_node, opts("Search"))
+    vim.keymap.set("n", "U", api.tree.toggle_custom_filter, opts("Toggle Hidden"))
+    vim.keymap.set("n", "W", api.tree.collapse_all, opts("Collapse"))
+    vim.keymap.set("n", "x", api.fs.cut, opts("Cut"))
+    vim.keymap.set("n", "y", api.fs.copy.filename, opts("Copy Name"))
+    vim.keymap.set("n", "Y", api.fs.copy.relative_path, opts("Copy Relative Path"))
+    vim.keymap.set("n", "<2-LeftMouse>", api.node.open.edit, opts("Open"))
+    vim.keymap.set("n", "<2-RightMouse>", api.tree.change_root_to_node, opts("CD"))
+  end,
   diagnostics = {
     enable = false,
-    show_on_dirs = false,
     icons = {
       hint = "",
       info = "",
       warning = "",
       error = "",
     },
-  },
-  filters = {
-    dotfiles = false,
-    custom = {},
-    exclude = {},
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 400,
   },
   actions = {
     use_system_clipboard = true,
@@ -193,58 +149,29 @@ require("nvim-tree").setup({
     },
     file_popup = {
       open_win_config = {
-        col = 0,
+        col = 1,
         row = 1,
         relative = "cursor",
         border = "single",
         style = "minimal",
       },
     },
-    open_file = {
-      quit_on_open = false,
-      resize_window = true,
-      window_picker = {
-        enable = true,
-        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-        exclude = {
-          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame", "incline", "fidget" },
-          buftype = { "nofile", "terminal", "help" },
-        },
-      },
-    },
   },
   trash = {
-    cmd = "trash -F",
+    cmd = vim.fn.has("macunix") == 1 and "trash -F" or "gio trash",
     require_confirm = true,
-  },
-  live_filter = {
-    prefix = "[FILTER]: ",
-    always_show_folders = false,
-  },
-  log = {
-    enable = false,
-    truncate = false,
-    types = {
-      all = false,
-      config = false,
-      copy_paste = false,
-      diagnostics = false,
-      git = false,
-      profile = false,
-    },
   },
 })
 
-local api = require("nvim-tree.api")
-local Event = require("nvim-tree.api").events.Event
-api.events.subscribe(Event.FileCreated, function(data)
-  vim.api.nvim_command("NvimTreeRefresh")
-  find_file.fn(data.fname)
-end)
-api.events.subscribe(Event.FolderCreated, function(data)
-  vim.api.nvim_command("NvimTreeRefresh")
-  find_file.fn(data.folder_name)
-end)
+-- local Event = require("nvim-tree.api").events.Event
+-- api.events.subscribe(Event.FileCreated, function(data)
+--   vim.api.nvim_command("NvimTreeRefresh")
+--   find_file.fn(data.fname)
+-- end)
+-- api.events.subscribe(Event.FolderCreated, function(data)
+--   vim.api.nvim_command("NvimTreeRefresh")
+--   find_file.fn(data.folder_name)
+-- end)
 
 -- vim.cmd([[highlight NvimTreeOpenedFile ctermfg=175 guifg=#d3869b]])
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -250,31 +250,31 @@ return require("packer").startup(function(use)
     config = [[require("config.dirbuf")]],
   })
 
-  -- use({
-  --   "nvim-tree/nvim-tree.lua",
-  --   config = [[require("config.nvim-tree")]],
-  -- }) -- file manager
+  use({
+    "nvim-tree/nvim-tree.lua",
+    config = [[require("config.nvim-tree")]],
+  }) -- file manager
 
   use({
     "is0n/fm-nvim",
     config = [[require("config.fm-nvim")]],
   })
 
-  use({
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "main",
-    -- branch = "v2.x",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      {
-        "s1n7ax/nvim-window-picker",
-        config = [[require("config.nvim-window-picker")]],
-      },
-    },
-    config = [[require("config.neo-tree")]],
-  })
+  -- use({
+  --   "nvim-neo-tree/neo-tree.nvim",
+  --   branch = "main",
+  --   -- branch = "v2.x",
+  --   requires = {
+  --     "nvim-lua/plenary.nvim",
+  --     "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+  --     "MunifTanjim/nui.nvim",
+  --     {
+  --       "s1n7ax/nvim-window-picker",
+  --       config = [[require("config.nvim-window-picker")]],
+  --     },
+  --   },
+  --   config = [[require("config.neo-tree")]],
+  -- })
   --}}}
 
   ----------------
