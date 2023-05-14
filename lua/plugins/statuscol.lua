53bd55d1a6a642295f770050abf41d71eef31191
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-05-14 11:04:09 -0400

    update(statuscol): config

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -5,16 +5,13 @@ return {
     require("statuscol").setup({
       relculright = true,
       segments = {
-        { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+        { text = { builtin.foldfunc }, click = "v:lua.ScFa", sign = { colwidth = 1 } },
         {
           sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true },
           click = "v:lua.ScSa",
         },
         { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
-        {
-          sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true },
-          click = "v:lua.ScSa",
-        },
+        { text = { "%s" }, click = "v:lua.ScSa" },
       },
     })
   end,
