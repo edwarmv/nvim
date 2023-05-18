f23a0c1cff4e2951de29d42bc5a6782cb76bbe44
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2023-05-18 14:50:29 -0400

    update: noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -11,8 +11,8 @@ return {
     },
     lsp = {
       override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
-        ["vim.lsp.util.stylize_markdown"] = false,
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
       },
       progress = {
         enabled = false,
