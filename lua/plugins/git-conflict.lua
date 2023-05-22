129dd778d4c57f80b0dcfaa96e9aa210bc23dfd3
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2023-05-22 08:58:59 -0400

    update: git-conflict

--- lua/plugins/git-conflict.lua
+++ lua/plugins/git-conflict.lua
@@ -2,12 +2,20 @@ return {
   "akinsho/git-conflict.nvim",
   version = "*",
   opts = {
-    default_mappings = true, -- disable buffer local mapping created by this plugin
+    default_mappings = false, -- disable buffer local mapping created by this plugin
     disable_diagnostics = true, -- This will disable the diagnostics in a buffer whilst it is conflicted
     highlights = { -- They must have background color, otherwise the default color will be used
       current = "DiffAdd",
       incoming = "DiffChange",
       ancestor = "Visual",
     },
+  },
+  keys = {
+    { "co", "<Plug>(git-conflict-ours)", desc = "Git Conflict - Choose Ours" },
+    { "ct", "<Plug>(git-conflict-theirs)", desc = "Git Conflict - Choose Theirs" },
+    { "cb", "<Plug>(git-conflict-both)", desc = "Git Conflict - Choose Both" },
+    { "c0", "<Plug>(git-conflict-none)", desc = "Git Conflict - Choose None" },
+    { "]x", "<Plug>(git-conflict-prev-conflict)", desc = "Git Conflict - Prev" },
+    { "[x", "<Plug>(git-conflict-next-conflict)", desc = "Git Conflict - Next" },
   },
 }
