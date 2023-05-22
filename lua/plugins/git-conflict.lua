601997c3ee7d7319f0a7beb04e78169a676f9ac2
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2023-05-22 17:06:13 -0400

    feat: update git-conflict

--- lua/plugins/git-conflict.lua
+++ lua/plugins/git-conflict.lua
@@ -1,21 +1,21 @@
 return {
   "akinsho/git-conflict.nvim",
   version = "*",
-  opts = {
-    default_mappings = false, -- disable buffer local mapping created by this plugin
-    disable_diagnostics = true, -- This will disable the diagnostics in a buffer whilst it is conflicted
-    highlights = { -- They must have background color, otherwise the default color will be used
-      current = "DiffAdd",
-      incoming = "DiffChange",
-      ancestor = "Visual",
-    },
-  },
-  keys = {
-    { "co", "<Plug>(git-conflict-ours)", desc = "Git Conflict - Choose Ours" },
-    { "ct", "<Plug>(git-conflict-theirs)", desc = "Git Conflict - Choose Theirs" },
-    { "cb", "<Plug>(git-conflict-both)", desc = "Git Conflict - Choose Both" },
-    { "c0", "<Plug>(git-conflict-none)", desc = "Git Conflict - Choose None" },
-    { "]x", "<Plug>(git-conflict-prev-conflict)", desc = "Git Conflict - Prev" },
-    { "[x", "<Plug>(git-conflict-next-conflict)", desc = "Git Conflict - Next" },
-  },
+  config = function()
+    require("git-conflict").setup({
+      default_mappings = false, -- disable buffer local mapping created by this plugin
+      disable_diagnostics = true, -- This will disable the diagnostics in a buffer whilst it is conflicted
+      highlights = { -- They must have background color, otherwise the default color will be used
+        current = "DiffAdd",
+        incoming = "DiffChange",
+        ancestor = "Visual",
+      },
+    })
+    vim.keymap.set("n", "<leader>gco", "<Plug>(git-conflict-ours)", { desc = "Git Conflict - Choose Ours" })
+    vim.keymap.set("n", "<leader>gct", "<Plug>(git-conflict-theirs)", { desc = "Git Conflict - Choose Theirs" })
+    vim.keymap.set("n", "<leader>gcb", "<Plug>(git-conflict-both)", { desc = "Git Conflict - Choose Both" })
+    vim.keymap.set("n", "<leader>gc0", "<Plug>(git-conflict-none)", { desc = "Git Conflict - Choose None" })
+    vim.keymap.set("n", "]x", "<Plug>(git-conflict-prev-conflict)", { desc = "Git Conflict - Prev" })
+    vim.keymap.set("n", "[x", "<Plug>(git-conflict-next-conflict)", { desc = "Git Conflict - Next" })
+  end,
 }
