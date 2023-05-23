dbb12913bc2179465f59bd102a58096cac93bc86
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2023-05-23 14:47:01 -0400

    feat: add emmet-vim cmp completion

--- lua/plugins/emmet-vim.lua
+++ lua/plugins/emmet-vim.lua
@@ -1,4 +1,29 @@
 return {
   "mattn/emmet-vim",
+  dependencies = { "hrsh7th/nvim-cmp", "dcampos/cmp-emmet-vim" },
+  config = function()
+    require("cmp").setup({
+      sources = {
+        {
+          name = "emmet_vim",
+          option = {
+            filetypes = {
+              "html",
+              "xml",
+              "typescriptreact",
+              "javascriptreact",
+              "css",
+              "sass",
+              "scss",
+              "less",
+              "heex",
+              "tsx",
+              "jsx",
+            },
+          },
+        },
+      },
+    })
+  end,
   event = "VimEnter",
 }
