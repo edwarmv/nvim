3b1753d385b2ec99935bd824dac89f8ad1a51c82
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2023-05-24 16:53:18 -0400

    update(emmet-vim): remove emmet_vim from cmp sources

--- lua/plugins/emmet-vim.lua
+++ lua/plugins/emmet-vim.lua
@@ -2,26 +2,37 @@ return {
   "mattn/emmet-vim",
   dependencies = { "hrsh7th/nvim-cmp", "dcampos/cmp-emmet-vim" },
   config = function()
-    require("cmp").setup({
-      sources = {
-        {
-          name = "emmet_vim",
-          option = {
-            filetypes = {
-              "html",
-              "xml",
-              "typescriptreact",
-              "javascriptreact",
-              "css",
-              "sass",
-              "scss",
-              "less",
-              "heex",
-              "tsx",
-              "jsx",
-            },
-          },
-        },
+    local cmp = require("cmp")
+    cmp.setup({
+      mapping = {
+        ["<c-x><c-e>"] = cmp.mapping({
+          i = function()
+            cmp.complete({
+              config = {
+                sources = {
+                  {
+                    name = "emmet_vim",
+                    option = {
+                      filetypes = {
+                        "html",
+                        "xml",
+                        "typescriptreact",
+                        "javascriptreact",
+                        "css",
+                        "sass",
+                        "scss",
+                        "less",
+                        "heex",
+                        "tsx",
+                        "jsx",
+                      },
+                    },
+                  },
+                },
+              },
+            })
+          end,
+        }),
       },
     })
   end,
