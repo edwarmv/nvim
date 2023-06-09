7e75f5a0cc45254d5d60593b3e826ef953c15e81
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-06-08 23:49:30 -0400

    feat(nvim-bqf): disable winblend & add fzf dependency

--- lua/plugins/nvim-bqf.lua
+++ lua/plugins/nvim-bqf.lua
@@ -1,10 +1,12 @@
 return {
   "kevinhwang91/nvim-bqf",
+  dependencies = { "junegunn/fzf" },
   ft = "qf",
   opts = {
     preview = {
       -- border_chars = { "│", "│", "─", "─", "┌", "┐", "└", "┘", "█" },
       border_chars = { "│", "│", "─", "─", "╭", "╮", "╰", "╯", "█" },
+      winblend = 0,
     },
   },
 }
