3fc603f3e73b33e4f84d19b8fc4b63646ce3eabe
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2023-06-09 17:27:42 -0400

    feat(git-conflict): add autocmd and fix mappings

--- lua/plugins/git-conflict.lua
+++ lua/plugins/git-conflict.lua
@@ -1,6 +1,7 @@
 return {
   "akinsho/git-conflict.nvim",
   version = "*",
+  lazy = false,
   config = function()
     require("git-conflict").setup({
       default_mappings = false, -- disable buffer local mapping created by this plugin
@@ -11,11 +12,23 @@ return {
         ancestor = "Visual",
       },
     })
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "GitConflictDetected",
+      callback = function()
+        vim.fn.execute("IndentBlanklineDisable")
+      end,
+    })
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "GitConflictResolved",
+      callback = function()
+        vim.fn.execute("IndentBlanklineEnable")
+      end,
+    })
     vim.keymap.set("n", "<leader>gco", "<Plug>(git-conflict-ours)", { desc = "Git Conflict - Choose Ours" })
     vim.keymap.set("n", "<leader>gct", "<Plug>(git-conflict-theirs)", { desc = "Git Conflict - Choose Theirs" })
     vim.keymap.set("n", "<leader>gcb", "<Plug>(git-conflict-both)", { desc = "Git Conflict - Choose Both" })
     vim.keymap.set("n", "<leader>gc0", "<Plug>(git-conflict-none)", { desc = "Git Conflict - Choose None" })
-    vim.keymap.set("n", "]x", "<Plug>(git-conflict-prev-conflict)", { desc = "Git Conflict - Prev" })
-    vim.keymap.set("n", "[x", "<Plug>(git-conflict-next-conflict)", { desc = "Git Conflict - Next" })
+    vim.keymap.set("n", "[x", "<Plug>(git-conflict-prev-conflict)", { desc = "Git Conflict - Prev" })
+    vim.keymap.set("n", "]x", "<Plug>(git-conflict-next-conflict)", { desc = "Git Conflict - Next" })
   end,
 }
