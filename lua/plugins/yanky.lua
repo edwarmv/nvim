f23496586aa6f988b6fecf988a14ca7441798f25
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2023-06-30 16:37:49 -0400

    feat(yanky): customize telescope mappings and add hydra

--- lua/plugins/yanky.lua
+++ lua/plugins/yanky.lua
@@ -9,9 +9,12 @@ return {
         exclude = { "ns", "nS", "xs", "xS" },
       },
     },
+    "anuvyklack/hydra.nvim",
   },
   config = function()
     require("telescope").load_extension("yank_history")
+    local mapping = require("yanky.telescope.mapping")
+    local utils = require("yanky.utils")
     require("yanky").setup({
       ring = {
         history_length = 10,
@@ -20,11 +23,23 @@ return {
         cancel_event = "update",
       },
       picker = {
-        select = {
-          action = nil, -- nil to use default put action
-        },
         telescope = {
-          mappings = nil, -- nil to use default mappings
+          use_default_mappings = false,
+          mappings = {
+            default = mapping.put("p"),
+            i = {
+              -- ["<c-p>"] = mapping.put("p"),
+              -- ["<c-k>"] = mapping.put("P"),
+              -- ["<c-x>"] = mapping.delete(),
+              ["<c-r>"] = mapping.set_register(utils.get_default_register()),
+            },
+            n = {
+              p = mapping.put("p"),
+              P = mapping.put("P"),
+              d = mapping.delete(),
+              r = mapping.set_register(utils.get_default_register()),
+            },
+          },
         },
       },
       system_clipboard = {
@@ -50,5 +65,61 @@ return {
 
     vim.keymap.set("n", "<c-n>", "<Plug>(YankyCycleForward)", {})
     vim.keymap.set("n", "<c-p>", "<Plug>(YankyCycleBackward)", {})
+
+    local Hydra = require("hydra")
+
+    local function t(str)
+      return vim.api.nvim_replace_termcodes(str, true, true, true)
+    end
+
+    local yanky_hydra = Hydra({
+      config = {
+        hint = {
+          type = "statusline",
+        },
+      },
+      name = "Yank ring",
+      mode = "n",
+      heads = {
+        { "p", "<Plug>(YankyPutAfter)", { desc = "After" } },
+        { "P", "<Plug>(YankyPutBefore)", { desc = "Before" } },
+        { "<C-n>", "<Plug>(YankyCycleForward)", { private = true, desc = "↓" } },
+        { "<C-p>", "<Plug>(YankyCycleBackward)", { private = true, desc = "↑" } },
+      },
+    })
+
+    -- choose/change the mappings if you want
+    for key, putAction in pairs({
+      ["p"] = "<Plug>(YankyPutAfter)",
+      ["P"] = "<Plug>(YankyPutBefore)",
+      ["gp"] = "<Plug>(YankyGPutAfter)",
+      ["gP"] = "<Plug>(YankyGPutBefore)",
+    }) do
+      vim.keymap.set({ "n", "x" }, key, function()
+        vim.fn.feedkeys(t(putAction))
+        yanky_hydra:activate()
+      end)
+    end
+
+    -- choose/change the mappings if you want
+    for key, putAction in pairs({
+      ["]p"] = "<Plug>(YankyPutIndentAfterLinewise)",
+      ["[p"] = "<Plug>(YankyPutIndentBeforeLinewise)",
+      ["]P"] = "<Plug>(YankyPutIndentAfterLinewise)",
+      ["[P"] = "<Plug>(YankyPutIndentBeforeLinewise)",
+
+      [">p"] = "<Plug>(YankyPutIndentAfterShiftRight)",
+      ["<p"] = "<Plug>(YankyPutIndentAfterShiftLeft)",
+      [">P"] = "<Plug>(YankyPutIndentBeforeShiftRight)",
+      ["<P"] = "<Plug>(YankyPutIndentBeforeShiftLeft)",
+
+      ["=p"] = "<Plug>(YankyPutAfterFilter)",
+      ["=P"] = "<Plug>(YankyPutBeforeFilter)",
+    }) do
+      vim.keymap.set("n", key, function()
+        vim.fn.feedkeys(t(putAction))
+        yanky_hydra:activate()
+      end)
+    end
   end,
 }
