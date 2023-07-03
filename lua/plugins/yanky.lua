58f8442a5a1b820419a5186976dadb78ab1f81b8
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-07-02 23:29:14 -0400

    feat(yanky): comment hydra integration

--- lua/plugins/yanky.lua
+++ lua/plugins/yanky.lua
@@ -9,7 +9,7 @@ return {
         exclude = { "ns", "nS", "xs", "xS" },
       },
     },
-    "anuvyklack/hydra.nvim",
+    -- "anuvyklack/hydra.nvim",
   },
   config = function()
     require("telescope").load_extension("yank_history")
@@ -66,60 +66,60 @@ return {
     vim.keymap.set("n", "<c-n>", "<Plug>(YankyCycleForward)", {})
     vim.keymap.set("n", "<c-p>", "<Plug>(YankyCycleBackward)", {})
 
-    local Hydra = require("hydra")
-
-    local function t(str)
-      return vim.api.nvim_replace_termcodes(str, true, true, true)
-    end
-
-    local yanky_hydra = Hydra({
-      config = {
-        hint = {
-          type = "statusline",
-        },
-      },
-      name = "Yank ring",
-      mode = "n",
-      heads = {
-        { "p", "<Plug>(YankyPutAfter)", { desc = "After" } },
-        { "P", "<Plug>(YankyPutBefore)", { desc = "Before" } },
-        { "<C-n>", "<Plug>(YankyCycleForward)", { private = true, desc = "↓" } },
-        { "<C-p>", "<Plug>(YankyCycleBackward)", { private = true, desc = "↑" } },
-      },
-    })
-
-    -- choose/change the mappings if you want
-    for key, putAction in pairs({
-      ["p"] = "<Plug>(YankyPutAfter)",
-      ["P"] = "<Plug>(YankyPutBefore)",
-      ["gp"] = "<Plug>(YankyGPutAfter)",
-      ["gP"] = "<Plug>(YankyGPutBefore)",
-    }) do
-      vim.keymap.set({ "n", "x" }, key, function()
-        vim.fn.feedkeys(t(putAction))
-        yanky_hydra:activate()
-      end)
-    end
-
-    -- choose/change the mappings if you want
-    for key, putAction in pairs({
-      ["]p"] = "<Plug>(YankyPutIndentAfterLinewise)",
-      ["[p"] = "<Plug>(YankyPutIndentBeforeLinewise)",
-      ["]P"] = "<Plug>(YankyPutIndentAfterLinewise)",
-      ["[P"] = "<Plug>(YankyPutIndentBeforeLinewise)",
-
-      [">p"] = "<Plug>(YankyPutIndentAfterShiftRight)",
-      ["<p"] = "<Plug>(YankyPutIndentAfterShiftLeft)",
-      [">P"] = "<Plug>(YankyPutIndentBeforeShiftRight)",
-      ["<P"] = "<Plug>(YankyPutIndentBeforeShiftLeft)",
-
-      ["=p"] = "<Plug>(YankyPutAfterFilter)",
-      ["=P"] = "<Plug>(YankyPutBeforeFilter)",
-    }) do
-      vim.keymap.set("n", key, function()
-        vim.fn.feedkeys(t(putAction))
-        yanky_hydra:activate()
-      end)
-    end
+    -- local Hydra = require("hydra")
+    --
+    -- local function t(str)
+    --   return vim.api.nvim_replace_termcodes(str, true, true, true)
+    -- end
+    --
+    -- local yanky_hydra = Hydra({
+    --   config = {
+    --     hint = {
+    --       type = "statusline",
+    --     },
+    --   },
+    --   name = "Yank ring",
+    --   mode = "n",
+    --   heads = {
+    --     { "p", "<Plug>(YankyPutAfter)", { desc = "After" } },
+    --     { "P", "<Plug>(YankyPutBefore)", { desc = "Before" } },
+    --     { "<C-n>", "<Plug>(YankyCycleForward)", { private = true, desc = "↓" } },
+    --     { "<C-p>", "<Plug>(YankyCycleBackward)", { private = true, desc = "↑" } },
+    --   },
+    -- })
+    --
+    -- -- choose/change the mappings if you want
+    -- for key, putAction in pairs({
+    --   ["p"] = "<Plug>(YankyPutAfter)",
+    --   ["P"] = "<Plug>(YankyPutBefore)",
+    --   ["gp"] = "<Plug>(YankyGPutAfter)",
+    --   ["gP"] = "<Plug>(YankyGPutBefore)",
+    -- }) do
+    --   vim.keymap.set({ "n", "x" }, key, function()
+    --     vim.fn.feedkeys(t(putAction))
+    --     yanky_hydra:activate()
+    --   end)
+    -- end
+    --
+    -- -- choose/change the mappings if you want
+    -- for key, putAction in pairs({
+    --   ["]p"] = "<Plug>(YankyPutIndentAfterLinewise)",
+    --   ["[p"] = "<Plug>(YankyPutIndentBeforeLinewise)",
+    --   ["]P"] = "<Plug>(YankyPutIndentAfterLinewise)",
+    --   ["[P"] = "<Plug>(YankyPutIndentBeforeLinewise)",
+    --
+    --   [">p"] = "<Plug>(YankyPutIndentAfterShiftRight)",
+    --   ["<p"] = "<Plug>(YankyPutIndentAfterShiftLeft)",
+    --   [">P"] = "<Plug>(YankyPutIndentBeforeShiftRight)",
+    --   ["<P"] = "<Plug>(YankyPutIndentBeforeShiftLeft)",
+    --
+    --   ["=p"] = "<Plug>(YankyPutAfterFilter)",
+    --   ["=P"] = "<Plug>(YankyPutBeforeFilter)",
+    -- }) do
+    --   vim.keymap.set("n", key, function()
+    --     vim.fn.feedkeys(t(putAction))
+    --     yanky_hydra:activate()
+    --   end)
+    -- end
   end,
 }
