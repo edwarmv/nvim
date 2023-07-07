0f02cca587f9570a2eec4822383184da6f98acd9
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2023-07-07 17:31:25 -0400

    feat(vim-illuminate): change providers order

--- lua/plugins/vim-illuminate.lua
+++ lua/plugins/vim-illuminate.lua
@@ -5,9 +5,9 @@ return {
     require("illuminate").configure({
       -- providers: provider used to get references in the buffer, ordered by priority
       providers = {
-        "lsp",
-        "treesitter",
         "regex",
+        "treesitter",
+        "lsp",
       },
       -- delay: delay in milliseconds
       delay = 300,
