51effc752a150ccd076c470d1fff889713660c6b
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2023-07-13 18:34:58 -0400

    feat(lsp_signature): disable hint

--- lua/plugins/lsp_signature.lua
+++ lua/plugins/lsp_signature.lua
@@ -29,7 +29,7 @@ return {
 
     close_timeout = 4000, -- close floating window after ms when laster parameter is entered
     fix_pos = false, -- set to true, the floating window will not auto-close until finish all parameters
-    hint_enable = true, -- virtual hint enable
+    hint_enable = false, -- virtual hint enable
     hint_prefix = " ", -- Panda for parameter, NOTE: for the terminal not support emoji, might crash
     hint_scheme = "String",
     hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
