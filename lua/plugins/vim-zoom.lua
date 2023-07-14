d619d17b8112a1f5c69750bf356b0e8acd295876
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2023-07-14 17:07:35 -0400

    feat(vim-zoom): enable plugin

--- lua/plugins/vim-zoom.lua
+++ lua/plugins/vim-zoom.lua
@@ -1,24 +1,23 @@
 return {
   "dhruvasagar/vim-zoom",
-  dependencies = { "nvim-zh/colorful-winsep.nvim" },
-  enabled = false,
-  lazy = false,
-  config = function()
-    local utils = require("utils.init")
-    local colorful_winsep = require("colorful-winsep")
-    utils.sourceVim("vim-zoom.vim")
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "ZoomPre",
-      callback = function()
-        colorful_winsep.NvimSeparatorDel()
-      end,
-    })
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "ZoomPost",
-      callback = function()
-        colorful_winsep.NvimSeparatorDel()
-        colorful_winsep.NvimSeparatorShow()
-      end,
-    })
-  end,
+  -- dependencies = { "nvim-zh/colorful-winsep.nvim" },
+  enabled = true,
+  -- config = function()
+  -- local utils = require("utils.init")
+  -- local colorful_winsep = require("colorful-winsep")
+  -- utils.sourceVim("vim-zoom.vim")
+  -- vim.api.nvim_create_autocmd("User", {
+  --   pattern = "ZoomPre",
+  --   callback = function()
+  --     colorful_winsep.NvimSeparatorDel()
+  --   end,
+  -- })
+  -- vim.api.nvim_create_autocmd("User", {
+  --   pattern = "ZoomPost",
+  --   callback = function()
+  --     colorful_winsep.NvimSeparatorDel()
+  --     colorful_winsep.NvimSeparatorShow()
+  --   end,
+  -- })
+  -- end,
 }
