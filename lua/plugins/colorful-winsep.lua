8ad78de1998c8b36e4a53173e508d2a286e2c4d4
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-07-15 10:39:54 -0400

    feat(colorful-winsep): try to use with vim.zoom

--- lua/plugins/colorful-winsep.lua
+++ lua/plugins/colorful-winsep.lua
@@ -6,6 +6,18 @@ return {
       fg = "#d3869b",
       bg = "#3c3836",
     },
+    interval = 3000,
+    create_event = function()
+      local zoomstatus = vim.fn["zoom#statusline"]()
+      print("colorful created " .. zoomstatus)
+      if zoomstatus == "[Z]" then
+        require("colorful-winsep").NvimSeparatorDel()
+      end
+    end,
+    close_event = function()
+      -- local zoomstatus = vim.fn["zoom#statusline"]()
+      --      print("colorful closed: "..zoomstatus)
+    end,
   },
   event = { "WinNew" },
 }
