4f6ebc76a05b9eb647842d8ba92620855a21ce23
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-07-17 13:55:51 -0400

    feat(emmet-vim): remove cmp dependencies

--- lua/plugins/emmet-vim.lua
+++ lua/plugins/emmet-vim.lua
@@ -1,40 +1,4 @@
 return {
   "mattn/emmet-vim",
-  dependencies = { "hrsh7th/nvim-cmp", "dcampos/cmp-emmet-vim" },
-  config = function()
-    local cmp = require("cmp")
-    cmp.setup({
-      mapping = {
-        ["<c-x><c-e>"] = cmp.mapping({
-          i = function()
-            cmp.complete({
-              config = {
-                sources = {
-                  {
-                    name = "emmet_vim",
-                    option = {
-                      filetypes = {
-                        "html",
-                        "xml",
-                        "typescriptreact",
-                        "javascriptreact",
-                        "css",
-                        "sass",
-                        "scss",
-                        "less",
-                        "heex",
-                        "tsx",
-                        "jsx",
-                      },
-                    },
-                  },
-                },
-              },
-            })
-          end,
-        }),
-      },
-    })
-  end,
   event = "VimEnter",
 }
