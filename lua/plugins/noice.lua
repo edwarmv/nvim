89cdf750ae4e97906471f3ec8079741111816072
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-07-18 23:20:41 -0400

    feat(noice): disable lsp treesitter overwrites

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -12,8 +12,8 @@ return {
     },
     lsp = {
       override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+        ["vim.lsp.util.stylize_markdown"] = false,
         ["cmp.entry.get_documentation"] = false,
       },
       progress = {
