e66b93311fb5a9c5615a8d63e1b75f3aae351bd3
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-07-23 10:34:35 -0400

    feat(custom): disable custom lsp format and rename

--- lua/config/maps.lua
+++ lua/config/maps.lua
@@ -1,5 +1,92 @@
 local M = {}
 
+local function lspRename()
+  local clients = vim.lsp.get_clients({
+    bufnr = 0,
+    method = "textDocument/rename",
+  })
+  vim.ui.select(clients, {
+    format_item = function(client)
+      return client.name
+    end,
+  }, function(chosenClient)
+    if chosenClient ~= nil then
+      vim.lsp.buf.rename(nil, {
+        filter = function(client)
+          return client.name == chosenClient.name
+        end,
+      })
+    end
+  end)
+end
+
+---@private
+---@param bufnr integer
+---@param mode "v"|"V"
+---@return table {start={row,col}, end={row,col}} using (1, 0) indexing
+local function range_from_selection(bufnr, mode)
+  -- TODO: Use `vim.region()` instead https://github.com/neovim/neovim/pull/13896
+
+  -- [bufnum, lnum, col, off]; both row and column 1-indexed
+  local start = vim.fn.getpos("v")
+  local end_ = vim.fn.getpos(".")
+  local start_row = start[2]
+  local start_col = start[3]
+  local end_row = end_[2]
+  local end_col = end_[3]
+
+  -- A user can start visual selection at the end and move backwards
+  -- Normalize the range to start < end
+  if start_row == end_row and end_col < start_col then
+    end_col, start_col = start_col, end_col
+  elseif end_row < start_row then
+    start_row, end_row = end_row, start_row
+    start_col, end_col = end_col, start_col
+  end
+  if mode == "V" then
+    start_col = 1
+    local lines = vim.api.nvim_buf_get_lines(bufnr, end_row - 1, end_row, true)
+    end_col = #lines[1]
+  end
+  return {
+    ["start"] = { start_row, start_col - 1 },
+    ["end"] = { end_row, end_col - 1 },
+  }
+end
+
+local function lspFormat()
+  local mode = vim.api.nvim_get_mode().mode
+  local range
+  if mode == "v" or mode == "V" then
+    range = range_from_selection(0, mode)
+  end
+
+  local method = range and "textDocument/rangeFormatting" or "textDocument/formatting"
+
+  local clients = vim.lsp.get_active_clients({
+    bufnr = 0,
+  })
+
+  clients = vim.tbl_filter(function(client)
+    return client.supports_method(method)
+  end, clients)
+
+  vim.ui.select(clients, {
+    format_item = function(client)
+      return client.name
+    end,
+  }, function(chosenClient)
+    if chosenClient ~= nil then
+      vim.lsp.buf.format({
+        filter = function(client)
+          return client.name == chosenClient.name
+        end,
+        range = range,
+      })
+    end
+  end)
+end
+
 M.lsp = function(bufnr)
   local opts = {
     buffer = bufnr,
@@ -26,15 +113,13 @@ M.lsp = function(bufnr)
 
   vim.keymap.set("n", "glt", vim.lsp.buf.type_definition, { desc = "[LSP] Type Definition" })
 
-  vim.keymap.set("n", "glr", vim.lsp.buf.rename, { desc = "[LSP] Rename" })
+  vim.keymap.set("n", "glr", lspRename, { desc = "[LSP] Rename" })
 
   vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "[LSP] Code Action" })
 
   vim.keymap.set("n", "glR", vim.lsp.buf.references, { desc = "[LSP] References" })
 
-  vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "[LSP] Format" })
-
-  vim.keymap.set("v", "<leader>f", vim.lsp.buf.format, { desc = "[LSP] Format" })
+  vim.keymap.set({ "n", "v" }, "<leader>f", lspFormat, { desc = "[LSP] Format" })
 
   vim.keymap.set("n", "<leader>d", function()
     vim.diagnostic.open_float({ border = "rounded" })
