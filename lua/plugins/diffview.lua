4736b1ca3bf44fa553c038f8398b4f9e4bc3b4f4
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-07-30 23:27:10 -0400

    chore: update diffview

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,4 +1,18 @@
 return {
   "sindrets/diffview.nvim",
-  dependencies = "nvim-lua/plenary.nvim",
+  config = function()
+    local Panel = require("diffview.ui.panel").Panel
+    Panel.default_config_float = {
+      type = "float",
+      relative = "editor",
+      row = 0,
+      col = 0,
+      zindex = 50,
+      style = "minimal",
+      border = "rounded",
+      win_opts = {},
+    }
+
+    require("diffview").setup()
+  end,
 }
