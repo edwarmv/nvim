3fa626efdea687940c466cecf81930996244e543
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-07-30 23:27:34 -0400

    chore: add neorg

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,29 @@
+return {
+  "nvim-neorg/neorg",
+  build = ":Neorg sync-parsers",
+  dependencies = { "nvim-lua/plenary.nvim", { "nvim-neorg/neorg-telescope" } },
+  config = function()
+    vim.opt.conceallevel = 3
+    require("neorg").setup({
+      load = {
+        ["core.defaults"] = {}, -- Loads default behaviour
+        ["core.integrations.telescope"] = {},
+        ["core.concealer"] = {}, -- Adds pretty icons to your documents
+        ["core.dirman"] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              main = "~/neorg",
+              knowledge = "~/neorg/knowledge",
+              work = "~/neorg/work",
+            },
+          },
+        },
+        ["core.completion"] = {
+          config = {
+            engine = "nvim-cmp",
+          },
+        },
+      },
+    })
+  end,
+}
