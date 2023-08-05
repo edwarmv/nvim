5d23906622c4e55564c3e2b5c1df0ed672bec8fb
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-08-05 11:48:06 -0400

    chore: add eol character

--- init.lua
+++ init.lua
@@ -203,10 +203,11 @@ vim.opt.termguicolors = true
 
 vim.diagnostic.config({
   -- underline = false,
-  virtual_text = true,
+  virtual_text = false,
   update_in_insert = true,
   signs = true,
   severity_sort = true,
+  virtual_lines = { only_current_line = true },
   -- float = {
   --   source = "always",  -- Or "if_many"
   -- },
@@ -233,7 +234,7 @@ vim.opt.listchars = {
   -- space = "⋅", -- "·"
   -- lead = "⋅",
   -- multispace = "+⋅",
-  -- eol = "¬",
+  eol = "↴",
   -- tab = "→ ",
   tab = "  ",
   -- tab = "-->",
@@ -323,56 +324,56 @@ vim.opt.laststatus = 3
 local fn = vim.fn
 
 function _G.qftf(info)
-    local items
-    local ret = {}
-    -- The name of item in list is based on the directory of quickfix window.
-    -- Change the directory for quickfix window make the name of item shorter.
-    -- It's a good opportunity to change current directory in quickfixtextfunc :)
-    --
-    -- local alterBufnr = fn.bufname('#') -- alternative buffer is the buffer before enter qf window
-    -- local root = getRootByAlterBufnr(alterBufnr)
-    -- vim.cmd(('noa lcd %s'):format(fn.fnameescape(root)))
-    --
-    if info.quickfix == 1 then
-        items = fn.getqflist({id = info.id, items = 0}).items
-    else
-        items = fn.getloclist(info.winid, {id = info.id, items = 0}).items
-    end
-    local limit = 31
-    local fnameFmt1, fnameFmt2 = '%-' .. limit .. 's', '…%.' .. (limit - 1) .. 's'
-    local validFmt = '%s │%5d:%-3d│%s %s'
-    for i = info.start_idx, info.end_idx do
-        local e = items[i]
-        local fname = ''
-        local str
-        if e.valid == 1 then
-            if e.bufnr > 0 then
-                fname = fn.bufname(e.bufnr)
-                if fname == '' then
-                    fname = '[No Name]'
-                else
-                    fname = fname:gsub('^' .. vim.env.HOME, '~')
-                end
-                -- char in fname may occur more than 1 width, ignore this issue in order to keep performance
-                if #fname <= limit then
-                    fname = fnameFmt1:format(fname)
-                else
-                    fname = fnameFmt2:format(fname:sub(1 - limit))
-                end
-            end
-            local lnum = e.lnum > 99999 and -1 or e.lnum
-            local col = e.col > 999 and -1 or e.col
-            local qtype = e.type == '' and '' or ' ' .. e.type:sub(1, 1):upper()
-            str = validFmt:format(fname, lnum, col, qtype, e.text)
+  local items
+  local ret = {}
+  -- The name of item in list is based on the directory of quickfix window.
+  -- Change the directory for quickfix window make the name of item shorter.
+  -- It's a good opportunity to change current directory in quickfixtextfunc :)
+  --
+  -- local alterBufnr = fn.bufname('#') -- alternative buffer is the buffer before enter qf window
+  -- local root = getRootByAlterBufnr(alterBufnr)
+  -- vim.cmd(('noa lcd %s'):format(fn.fnameescape(root)))
+  --
+  if info.quickfix == 1 then
+    items = fn.getqflist({ id = info.id, items = 0 }).items
+  else
+    items = fn.getloclist(info.winid, { id = info.id, items = 0 }).items
+  end
+  local limit = 31
+  local fnameFmt1, fnameFmt2 = "%-" .. limit .. "s", "…%." .. (limit - 1) .. "s"
+  local validFmt = "%s │%5d:%-3d│%s %s"
+  for i = info.start_idx, info.end_idx do
+    local e = items[i]
+    local fname = ""
+    local str
+    if e.valid == 1 then
+      if e.bufnr > 0 then
+        fname = fn.bufname(e.bufnr)
+        if fname == "" then
+          fname = "[No Name]"
         else
-            str = e.text
+          fname = fname:gsub("^" .. vim.env.HOME, "~")
         end
-        table.insert(ret, str)
+        -- char in fname may occur more than 1 width, ignore this issue in order to keep performance
+        if #fname <= limit then
+          fname = fnameFmt1:format(fname)
+        else
+          fname = fnameFmt2:format(fname:sub(1 - limit))
+        end
+      end
+      local lnum = e.lnum > 99999 and -1 or e.lnum
+      local col = e.col > 999 and -1 or e.col
+      local qtype = e.type == "" and "" or " " .. e.type:sub(1, 1):upper()
+      str = validFmt:format(fname, lnum, col, qtype, e.text)
+    else
+      str = e.text
     end
-    return ret
+    table.insert(ret, str)
+  end
+  return ret
 end
 
-vim.o.qftf = '{info -> v:lua._G.qftf(info)}'
+vim.o.qftf = "{info -> v:lua._G.qftf(info)}"
 
 -- local cmdheight
 
