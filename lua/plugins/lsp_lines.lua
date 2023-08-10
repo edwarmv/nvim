eb7e07f88ecf4b62884bb81ccde1c448e8b5089e
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2023-08-10 16:24:41 -0400

    chore: add toggle mapping to lsp_lines

--- lua/plugins/lsp_lines.lua
+++ lua/plugins/lsp_lines.lua
@@ -3,5 +3,14 @@ return {
   depencies = { "neovim/nvim-lspconfig" },
   config = function()
     require("lsp_lines").setup()
+    local enabled = true;
+    vim.keymap.set("", "<Leader>D", function()
+      enabled = not enabled
+      if enabled then
+        vim.diagnostic.config({ virtual_lines = { only_current_line = enabled } })
+      else
+        vim.diagnostic.config({ virtual_lines = false })
+      end
+    end, { desc = "[lsp_lines] - Toggle" })
   end,
 }
