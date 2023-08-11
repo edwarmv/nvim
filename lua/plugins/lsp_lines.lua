f6a6d8be181cd5c2a0f285184b234eae02e7a65a
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-08-10 22:44:40 -0400

    chore: lsp_lines disable on insert

--- lua/plugins/lsp_lines.lua
+++ lua/plugins/lsp_lines.lua
@@ -3,14 +3,31 @@ return {
   depencies = { "neovim/nvim-lspconfig" },
   config = function()
     require("lsp_lines").setup()
-    local enabled = true;
+    local enabled = true
     vim.keymap.set("", "<Leader>D", function()
       enabled = not enabled
       if enabled then
-        vim.diagnostic.config({ virtual_lines = { only_current_line = enabled } })
+        vim.diagnostic.config({ virtual_lines = { only_current_line = true } })
       else
         vim.diagnostic.config({ virtual_lines = false })
       end
     end, { desc = "[lsp_lines] - Toggle" })
+
+    local group = vim.api.nvim_create_augroup("LspLinesToggleInsert", { clear = false })
+    vim.api.nvim_create_autocmd("InsertEnter", {
+      group = group,
+      callback = function()
+        vim.diagnostic.config({ virtual_lines = false })
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("InsertLeave", {
+      group = group,
+      callback = function()
+        if enabled then
+          vim.diagnostic.config({ virtual_lines = { only_current_line = true } })
+        end
+      end,
+    })
   end,
 }
