bafd8d381be96e7f82476d45f2f67ef567df9936
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2023-08-16 16:39:17 -0400

    feat(colorful-winsep): enable

--- lua/plugins/colorful-winsep.lua
+++ lua/plugins/colorful-winsep.lua
@@ -1,23 +1,11 @@
 return {
   "nvim-zh/colorful-winsep.nvim",
-  enabled = false,
+  enabled = true,
   opts = {
     highlight = {
-      fg = "#d3869b",
-      bg = "#3c3836",
+      fg = "#29a4bd",
+      bg = "#24283b",
     },
-    interval = 3000,
-    create_event = function()
-      local zoomstatus = vim.fn["zoom#statusline"]()
-      print("colorful created " .. zoomstatus)
-      if zoomstatus == "[Z]" then
-        require("colorful-winsep").NvimSeparatorDel()
-      end
-    end,
-    close_event = function()
-      -- local zoomstatus = vim.fn["zoom#statusline"]()
-      --      print("colorful closed: "..zoomstatus)
-    end,
   },
   event = { "WinNew" },
 }
