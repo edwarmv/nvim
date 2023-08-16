5f995a8590c0b690cec1377d734c3a99e99fdc88
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2023-08-16 16:42:07 -0400

    feat: add zen-mode plugin

---
+++ lua/plugins/zen-mode.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/zen-mode.nvim",
+  opts = {
+    window = {
+      width = 0.85,
+    },
+  },
+  keys = {
+    { "<leader>z", "<cmd>ZenMode<cr>", desc = "Toggle ZenMode" },
+  },
+}
