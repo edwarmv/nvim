082c8a66c58431d3309495d02a032c2d1efbc0be
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2023-08-17 09:15:40 -0400

    feat(vim-zoom): enable

--- lua/plugins/vim-zoom.lua
+++ lua/plugins/vim-zoom.lua
@@ -1,7 +1,14 @@
 return {
   "dhruvasagar/vim-zoom",
   -- dependencies = { "nvim-zh/colorful-winsep.nvim" },
-  enabled = false,
+  enabled = true,
+  init = function()
+    vim.g["zoom#statustext"] = "[Z]"
+    vim.g.zoom_tmux_z = false
+  end,
+  keys = {
+    { "<leader>z", "<Plug>(zoom-toggle)", desc = "Toggle Zoom" },
+  },
   -- config = function()
   -- local utils = require("utils.init")
   -- local colorful_winsep = require("colorful-winsep")
