14cd6d292035f23abf4eab70fb1a5c0afda35447
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2023-09-20 14:23:31 -0400

    feat(statuscol): update

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -3,15 +3,29 @@ return {
   config = function()
     local builtin = require("statuscol.builtin")
     require("statuscol").setup({
-      relculright = true,
+      ft_ignore = { "neo-tree", "Trouble" },
+      relculright = false,
       segments = {
-        { text = { builtin.foldfunc }, click = "v:lua.ScFa", sign = { colwidth = 1 } },
+        { text = { builtin.foldfunc }, click = "v:lua.ScFa", sign = { colwidth = 1, auto = true } },
         {
-          sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true },
+          text = {
+            builtin.lnumfunc,
+            -- function()
+            --   return (vim.b.gitsigns_status == "" or vim.b.gitsigns_status == nil) and " " or ""
+            -- end
+          },
+          sign = { auto = true },
+          click = "v:lua.ScLa",
+        },
+        {
+          sign = { name = { "GitSigns" }, maxwidth = 1, auto = true },
           click = "v:lua.ScSa",
         },
-        { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
-        { text = { "%s" }, click = "v:lua.ScSa" },
+        {
+          sign = { name = { "Diagnostic" }, maxwidth = 1, colwidth = 2, auto = true },
+          click = "v:lua.ScSa",
+        },
+        -- { text = { "%s" }, click = "v:lua.ScSa" },
       },
     })
   end,
