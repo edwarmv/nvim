0e6c0cf4f7b0b251b1b06263144c3850a6392ad6
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2023-09-20 14:23:23 -0400

    feat: add nvim-lightbulb

---
+++ lua/plugins/nvim-lightbulb.lua
@@ -0,0 +1,16 @@
+return {
+  "kosayoda/nvim-lightbulb",
+  opts = {
+    autocmd = { enabled = true },
+    sign = {
+      enabled = false,
+    },
+    float = {
+      enabled = false,
+    },
+    virtual_text = {
+      enabled = true,
+      hl = "",
+    },
+  },
+}
