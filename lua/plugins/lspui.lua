a4fa74bbe930c9cab4b48e2795b752e693e3dcfd
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2023-09-20 14:22:43 -0400

    feat: add lspui, but disabled

---
+++ lua/plugins/lspui.lua
@@ -0,0 +1,18 @@
+return {
+  "jinzhongjia/LspUI.nvim",
+  enabled = false,
+  branch = "main",
+  config = function()
+    require("LspUI").setup({
+      hover = {
+        command_enable = true,
+      },
+      lightbulb = {
+        enable = false,
+      },
+      inlay_hint = {
+        enable = false,
+      }
+    })
+  end,
+}
