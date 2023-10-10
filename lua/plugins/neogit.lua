cade667972feda408ac8eff5454ba9368dc77888
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2023-10-10 16:36:43 -0400

    feat: add neogit plugin

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,21 @@
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim", -- required
+    "nvim-telescope/telescope.nvim", -- optional
+    "sindrets/diffview.nvim", -- optional
+    "ibhagwan/fzf-lua", -- optional
+  },
+  opts = {
+    signs = {
+      -- { CLOSED, OPENED }
+      hunk = { "", "" },
+      item = { "", "" },
+      section = { "", "" },
+    },
+  },
+  keys = {
+    { "<leader>n", "<cmd>Neogit<cr>", desc = "[Neogit] - Open" },
+    { "<leader>N", "<cmd>Neogit cwd=%:p:h<cr>", desc = "[Neogit] - Open Relative" },
+  },
+}
