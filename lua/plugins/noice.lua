6f4d2bc5cbdbbe7d370c7d96e173cc78f2ed8c05
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-10-24 00:02:38 -0400

    feat(noice): enable markdown override and disable status

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -13,13 +13,13 @@ return {
       view_search = false,
     },
     lsp = {
-      -- override = {
-      --   ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
-      --   ["vim.lsp.util.stylize_markdown"] = false,
-      --   ["cmp.entry.get_documentation"] = false,
-      -- },
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
+      },
       progress = {
-        enabled = true,
+        enabled = false,
       },
       hover = {
         enabled = true,
