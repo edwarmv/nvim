d66c8906a6f787ce388f124b9455736602095abc
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-12-07 00:53:03 -0400

    feat: update statuscol

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -11,7 +11,7 @@ return {
           sign = { name = { "Dap*" }, maxwidth = 1, colwidth = 1, auto = true },
           click = "v:lua.ScLa",
         },
-        { text = { builtin.foldfunc }, click = "v:lua.ScFa", sign = { colwidth = 1, auto = true } },
+        { text = { builtin.foldfunc, " " }, click = "v:lua.ScFa", sign = { colwidth = 1, auto = true } },
         {
           sign = { name = { "todo*" }, maxwidth = 1, colwidth = 2, auto = true },
         },
