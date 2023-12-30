40685129f324548fed17a75ff82218ed74a84acb
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-12-30 09:08:28 -0400

    feat; update cmp

--- lua/plugins/nvim-cmp/init.lua
+++ lua/plugins/nvim-cmp/init.lua
@@ -247,7 +247,7 @@ return {
         }),
       }, --}}}
       sources = cmp.config.sources({
-        { name = "nvim_lsp", max_item_count = 100 },
+        { name = "nvim_lsp", max_item_count = 50 },
         { name = "buffer" },
       }),
       formatting = { --{{{
