151720d6bca4b17caf9c365daea1e59d11a070a6
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-01-04 16:03:40 -0400

    feat: disable statuscol

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -2,7 +2,9 @@ return {
   "folke/todo-comments.nvim",
   dependencies = "nvim-lua/plenary.nvim",
   config = function()
-    require("todo-comments").setup({})
+    require("todo-comments").setup({
+      signs = false,
+    })
 
     vim.keymap.set("n", "<space>tt", "<cmd>TodoTelescope<cr>")
   end,
