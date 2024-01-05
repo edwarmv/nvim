615841355f1aa73dc35fb376ceacefe7e9b828d9
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2024-01-04 23:32:13 -0400

    feat(lsp): update tailwindcss

--- lua/lsp/tailwindcss.lua
+++ lua/lsp/tailwindcss.lua
@@ -2,4 +2,9 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 require("lspconfig").tailwindcss.setup({
   capabilities = capabilities,
+  settings = {
+    tailwindCSS = {
+      suggestions = false,
+    },
+  },
 })
