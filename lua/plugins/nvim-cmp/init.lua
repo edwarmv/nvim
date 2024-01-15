8d2e9d4e9f7dbdfc4b0b92b38055d847b8071360
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2024-01-15 00:40:45 -0400

    feat: update cmp

--- lua/plugins/nvim-cmp/init.lua
+++ lua/plugins/nvim-cmp/init.lua
@@ -215,7 +215,6 @@ return {
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "luasnip" },
-      }, {
         { name = "buffer" },
       }),
       formatting = { --{{{
