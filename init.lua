4700df8026a5f443827b6759aa8cab2fbf95dc33
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-01-29 07:41:45 -0400

    feat: update opts

--- init.lua
+++ init.lua
@@ -17,11 +17,11 @@ vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
 
 local opt = vim.opt
 
-opt.autowrite = true -- Enable auto write
+opt.autowrite = false -- Enable auto write
 opt.clipboard = "unnamedplus" -- Sync with system clipboard
 opt.completeopt = "menu,menuone,noselect"
 opt.conceallevel = 2 -- Hide * markup for bold and italic, but not markers with substitutions
-opt.confirm = true -- Confirm to save changes before exiting modified buffer
+opt.confirm = false -- Confirm to save changes before exiting modified buffer
 opt.cursorline = true -- Enable highlighting of the current line
 opt.expandtab = true -- Use spaces instead of tabs
 opt.formatoptions = "jcroqlnt" -- tcqj
