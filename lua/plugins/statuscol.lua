d28cd9702980b4716a305aeb25de775d5c6fe9d9
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-02-19 16:18:40 -0400

    feat(statuscol): update config

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -24,15 +24,15 @@ return {
           click = "v:lua.ScSa",
         },
         {
-          sign = { namespace = { "gitsigns" }, maxwidth = 1, colwidth = 1, auto = true, wrap = true },
-          click = "v:lua.ScSa",
-        },
-        {
           text = {
             builtin.lnumfunc,
           },
           sign = { auto = true },
           click = "v:lua.ScLa",
+        },
+        {
+          sign = { namespace = { "gitsigns" }, maxwidth = 1, colwidth = 1, auto = true, wrap = true },
+          click = "v:lua.ScSa",
         },
         { text = { " " } },
         -- { text = { "%s" }, click = "v:lua.ScSa" },
