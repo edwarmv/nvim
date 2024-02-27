8397597469001af1c3b48b8945787ed81f8674bf
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-02-27 10:41:33 -0400

    feat: use default segments for statuscol

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -4,55 +4,39 @@ return {
   config = function()
     local builtin = require("statuscol.builtin")
     require("statuscol").setup({
-      ft_ignore = { "neo-tree", "Trouble", "NeogitStatus", "NvimTree" },
+      -- ft_ignore = { "neo-tree", "Trouble", "NeogitStatus", "NvimTree" },
       relculright = true,
-      segments = {
-        {
-          text = { builtin.foldfunc },
-          click = "v:lua.ScFa",
-          sign = { colwidth = 1, auto = true },
-        },
-        {
-          sign = { name = { "todo*" }, maxwidth = 1, colwidth = 2, auto = true },
-        },
-        {
-          sign = { namespace = { "dap" }, maxwidth = 1, colwidth = 2, auto = true },
-          click = "v:lua.ScLa",
-        },
-        {
-          sign = { namespace = { "diagnostic" }, maxwidth = 1, colwidth = 2, auto = true },
-          click = "v:lua.ScSa",
-        },
-        {
-          text = {
-            builtin.lnumfunc,
-          },
-          sign = { auto = true },
-          click = "v:lua.ScLa",
-        },
-        {
-          sign = { namespace = { "gitsigns" }, maxwidth = 1, colwidth = 1, auto = true, wrap = true },
-          click = "v:lua.ScSa",
-        },
-        { text = { " " } },
-        -- { text = { "%s" }, click = "v:lua.ScSa" },
-      },
-    })
-
-    local id = vim.api.nvim_create_augroup("InsertRedraw", {})
-    vim.api.nvim_create_autocmd("InsertEnter", {
-      group = id,
-      callback = function()
-        _G.insert = true
-        vim.cmd("redraw!")
-      end,
-    })
-    vim.api.nvim_create_autocmd("InsertLeave", {
-      group = id,
-      callback = function()
-        _G.insert = false
-        vim.cmd("redraw!")
-      end,
+      -- segments = {
+      --   {
+      --     sign = { name = { "todo*" }, maxwidth = 1, colwidth = 2, auto = true },
+      --   },
+      --   {
+      --     sign = { namespace = { "dap" }, maxwidth = 1, colwidth = 2, auto = true },
+      --     click = "v:lua.ScLa",
+      --   },
+      --   {
+      --     sign = { namespace = { "diagnostic" }, maxwidth = 1, colwidth = 2, auto = true },
+      --     click = "v:lua.ScSa",
+      --   },
+      --   {
+      --     text = {
+      --       builtin.lnumfunc,
+      --     },
+      --     sign = { auto = true },
+      --     click = "v:lua.ScLa",
+      --   },
+      --   {
+      --     sign = { namespace = { "gitsigns" }, maxwidth = 1, colwidth = 1, auto = true, wrap = true },
+      --     click = "v:lua.ScSa",
+      --   },
+      --   {
+      --     text = { builtin.foldfunc },
+      --     click = "v:lua.ScFa",
+      --     sign = { colwidth = 1, auto = true },
+      --   },
+      --   { text = { " " } },
+      --   -- { text = { "%s" }, click = "v:lua.ScSa" },
+      -- },
     })
   end,
 }
