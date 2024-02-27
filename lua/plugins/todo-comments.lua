205fed6801fd3fcaddbfc584f33c7c376741c1c8
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-02-27 10:41:58 -0400

    feat: add mapping to open todo-comments in trouble split

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,11 +1,12 @@
 return {
   "folke/todo-comments.nvim",
-  dependencies = "nvim-lua/plenary.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "folke/trouble.nvim" },
   config = function()
     require("todo-comments").setup({
       signs = true,
     })
 
     vim.keymap.set("n", "<space>tt", "<cmd>TodoTelescope<cr>")
+    vim.keymap.set("n", "<leader>xt", "<cmd>TodoTrouble<cr>")
   end,
 }
