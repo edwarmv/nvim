5c5af218d79ae92a825d334a3ac7acfdc00c6a9c
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-04-11 15:31:03 -0400

    feat: update smart-splits mappings

--- lua/plugins/smart-splits.lua
+++ lua/plugins/smart-splits.lua
@@ -5,13 +5,13 @@ return {
       cursor_follows_swapped_bufs = true,
     })
 
-    vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left, { desc = "Move Cursor Left"})
-    vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down, { desc = "Move Cursor Down"})
-    vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up, { desc = "Move Cursor Up"})
-    vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right, { desc = "Move Cursor Right"})
-    vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left, { desc = "Swap Buf Left"})
-    vim.keymap.set("n", "<leader><leader>j", require("smart-splits").swap_buf_down, { desc = "Swap Buf Down"})
-    vim.keymap.set("n", "<leader><leader>k", require("smart-splits").swap_buf_up, { desc = "Swap Buf Up"})
-    vim.keymap.set("n", "<leader><leader>l", require("smart-splits").swap_buf_right, { desc = "Swap Buf Right"})
+    vim.keymap.set("n", "<C-w>h", require("smart-splits").move_cursor_left, { desc = "Move Cursor Left"})
+    vim.keymap.set("n", "<C-w>j", require("smart-splits").move_cursor_down, { desc = "Move Cursor Down"})
+    vim.keymap.set("n", "<C-w>k", require("smart-splits").move_cursor_up, { desc = "Move Cursor Up"})
+    vim.keymap.set("n", "<C-w>l", require("smart-splits").move_cursor_right, { desc = "Move Cursor Right"})
+    vim.keymap.set("n", "<C-w>H", require("smart-splits").swap_buf_left, { desc = "Swap Buf Left"})
+    vim.keymap.set("n", "<C-w>J", require("smart-splits").swap_buf_down, { desc = "Swap Buf Down"})
+    vim.keymap.set("n", "<C-w>K", require("smart-splits").swap_buf_up, { desc = "Swap Buf Up"})
+    vim.keymap.set("n", "<C-w>L", require("smart-splits").swap_buf_right, { desc = "Swap Buf Right"})
   end,
 }
