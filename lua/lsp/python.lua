af28fb47d45b83428086fda06fd89f9324b00f52
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-04-16 16:34:21 -0400

    feat: update python lsp

--- lua/lsp/python.lua
+++ lua/lsp/python.lua
@@ -6,4 +6,18 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 require("lspconfig").basedpyright.setup({
   capabilities = capabilities,
+  settings = {
+    basedpyright = {
+      analysis = {
+        autoSearchPaths = true,
+        diagnosticMode = "openFilesOnly",
+        useLibraryCodeForTypes = true,
+        typeCheckingMode = "standard", -- ["off", "basic", "standard", "strict", "all"]
+        -- diagnosticSeverityOverrides = {
+        --   reportMissingTypeStubs = "warning",
+        --   reportUnknownMemberType = "warning",
+        -- },
+      },
+    },
+  },
 })
