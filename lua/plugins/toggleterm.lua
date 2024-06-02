dc93b70ac1267c29ae4e1465c536c6cbbc1be3ea
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2024-06-02 19:35:43 -0400

    update options of toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,7 +4,6 @@ return {
   config = function()
     local defaults = require("config.defaults")
     require("toggleterm").setup({
-      -- size can be a number or function which is passed the current terminal
       size = function(term)
         if term.direction == "horizontal" then
           return 0.2 * (vim.o.lines - vim.o.cmdheight)
@@ -13,11 +12,7 @@ return {
         end
       end,
       open_mapping = [[<m-\>]],
-      -- on_open = fun(t: Terminal), -- function to run when the terminal opens
-      -- on_close = fun(t: Terminal), -- function to run when the terminal closes
       highlights = {
-        -- highlights which map to a highlight group name and a table of it's values
-        -- NOTE: this is only a subset of values, any group placed here will be set for the terminal window split
         NormalFloat = {
           link = "NormalFloat",
         },
@@ -25,33 +20,22 @@ return {
           link = "FloatBorder",
         },
       },
-      hide_numbers = true, -- hide the number column in toggleterm buffers
-      shade_filetypes = {},
+      direction = "horizontal", -- 'vertical' | 'horizontal' | 'window' | 'float',
       shade_terminals = false,
-      -- shading_factor = "<number>", -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
-      start_in_insert = true,
-      insert_mappings = true, -- whether or not the open mapping applies in insert mode
-      terminal_mappings = false, -- whether or not the open mapping applies in the opened terminals
-      persist_size = true,
-      direction = "float", -- 'vertical' | 'horizontal' | 'window' | 'float',
-      close_on_exit = true, -- close the terminal window when the process exits
-      shell = vim.o.shell, -- change the default shell
       float_opts = {
         border = defaults.border,
         width = function(_term)
           local width = math.ceil(vim.o.columns * 0.9)
-          -- _term.float_opts.col = vim.o.columns
           return width
         end,
         height = function(_term)
           local height = math.ceil(vim.o.lines * 0.8)
-          -- _term.float_opts.row = math.ceil(vim.o.lines - 2 / 2)
           return height
         end,
       },
+      winbar = {
+        enabled = true,
+      },
     })
-
-    -- vim.keymap.set("t", "<c-\\>", "<c-\\><c-n>")
-    vim.cmd("autocmd! TermOpen term://* tmap <m-\\> <cmd>ToggleTerm<cr>")
   end,
 }
