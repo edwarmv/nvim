d5c28fe49d541e25b81a08d37f3715ea675001d9
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2024-06-05 23:29:29 -0400

    update

--- init.lua
+++ init.lua
@@ -169,7 +169,10 @@ vim.keymap.set("i", "<s-tab>", "<c-d>")
 
 vim.diagnostic.config({
   -- underline = false,
-  virtual_text = true,
+  virtual_text = false,
+  virtual_improved = {
+    current_line = "only",
+  },
   update_in_insert = false,
   severity_sort = true,
   virtual_lines = { only_current_line = true },
