44dff38a62c37224a440b9438e4581e25fe18028
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-06-20 07:59:07 -0400

    update gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -48,6 +48,14 @@ return {
       changedelete = { text = "▌" },
       untracked = { text = "▌" },
     },
+    signs_staged = {
+      add = { text = "▌" },
+      change = { text = "▌" },
+      delete = { text = "▌" },
+      topdelete = { text = "▌" },
+      changedelete = { text = "▌" },
+      untracked = { text = "▌" },
+    },
     signcolumn = true,
     preview_config = {
       border = defaults.border,
