642ebb2fd02b024db8c1500fef49c60d0d30d8a3
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-06-20 15:38:27 -0400

    update yanky disable sqlite

--- lua/plugins/yanky.lua
+++ lua/plugins/yanky.lua
@@ -1,7 +1,7 @@
 return {
   "gbprod/yanky.nvim",
   dependencies = {
-    "kkharji/sqlite.lua",
+    -- "kkharji/sqlite.lua",
     {
       "gbprod/cutlass.nvim",
       opts = {
@@ -17,11 +17,7 @@ return {
     local utils = require("yanky.utils")
     require("yanky").setup({
       ring = {
-        storage = "sqlite",
-      },
-      highlight = {
-        on_put = false,
-        on_yank = false,
+        storage = "shada",
       },
     })
     require("telescope").load_extension("yank_history")
