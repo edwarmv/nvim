42db1024d0a575ca359a3682fbbebf8a393c7caf
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2024-07-13 11:02:56 -0400

    update which-key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,37 +1,18 @@
-local defaults = require("config.defaults")
-
 return {
   "folke/which-key.nvim",
-  config = function()
-    require("which-key").setup({
-      plugins = {
-        marks = true, -- shows a list of your marks on ' and `
-        registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
-        spelling = {
-          enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-          suggestions = 20, -- how many suggestions should be shown in the list?
-        },
-        -- the presets plugin, adds help for a bunch of default keybindings in Neovim
-        -- No actual key bindings are created
-        presets = {
-          operators = true, -- adds help for operators like d, y, ... and registers them for motion / text object completion
-          motions = true, -- adds help for motions
-          text_objects = true, -- help for text objects triggered after entering an operator
-          windows = true, -- default bindings on <c-w>
-          nav = true, -- misc bindings to work with windows
-          z = true, -- bindings for folds, spelling and others prefixed with z
-          g = true, -- bindings for prefixed with g
-        },
-      },
-      -- triggers_blacklist = {
-      --   n = { "y", "v" },
-      -- },
-      window = {
-        border = "none",
-      },
-    })
-    -- local presets = require("which-key.plugins.presets")
-    -- presets.operators["v"] = nil
-    -- presets.operators["y"] = nil
-  end,
+  event = "VeryLazy",
+  opts = {
+    delay = function(ctx)
+      return ctx.plugin and 0 or 500
+    end,
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
 }
