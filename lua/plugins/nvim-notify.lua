db871dbe8f129d22bb62ce92c9ca4b39883de05e
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2024-07-13 21:16:29 -0400

    update

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -26,11 +26,11 @@ return {
       timeout = 3000,
 
       -- Max number of columns for messages
-      max_width = function()
-        return math.floor(vim.opt.columns:get() * 0.35)
-      end,
+      -- max_width = function()
+      --   return math.floor(vim.opt.columns:get() * 0.35)
+      -- end,
       -- Max number of lines for a message
-      max_height = 1,
+      -- max_height = 1,
 
       -- For stages that change opacity this is treated as the highlight behind the window
       -- Set this to either a highlight group, an RGB hex value e.g. "#000000" or a function returning an RGB code for dynamic values
@@ -38,7 +38,7 @@ return {
       -- background_colour = "Normal",
 
       -- Minimum width for notification windows
-      minimum_width = 0,
+      -- minimum_width = 0,
 
       -- Icons for the different levels
       icons = {
