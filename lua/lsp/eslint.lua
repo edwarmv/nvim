fc6e774bea09b0d0b2be0128e129e387096b7437
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2024-07-30 18:18:59 -0400

    update eslint

--- lua/lsp/eslint.lua
+++ lua/lsp/eslint.lua
@@ -2,4 +2,10 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 require("lspconfig").eslint.setup({
   capabilities = capabilities,
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end,
 })
