03a036be9ed619d382e1b1da80f5c14677d6443f
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-08-30 08:52:54 -0400

    update glance

--- lua/plugins/glance.lua
+++ lua/plugins/glance.lua
@@ -1,9 +1,15 @@
 return {
   "dnlhc/glance.nvim",
   opts = {
+    preview_win_opts = {
+      wrap = false,
+    },
     folds = {
       fold_closed = "▸",
       fold_open = "▾",
+    },
+    indent_lines = {
+      enable = false,
     },
   },
 }
