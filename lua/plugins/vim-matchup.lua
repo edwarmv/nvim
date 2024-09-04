11912747b7b6f27da1b2f829dfe6aeddffa1e248
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-09-04 09:29:59 -0400

    use my fork of vim-matchup temporarily

--- lua/plugins/vim-matchup.lua
+++ lua/plugins/vim-matchup.lua
@@ -1,6 +1,7 @@
 return {
-  "andymass/vim-matchup",
-  enabled = false,
+  -- "andymass/vim-matchup",
+  "edwarmv/vim-matchup",
+  enabled = true,
   init = function()
     vim.g.matchup_matchparen_offscreen = { method = "" }
     -- vim.g.matchup_text_obj_enabled = false
