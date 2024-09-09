1713707859a40598e784235ad00d08bc998f3f87
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-09-09 16:06:12 -0400

    update lazy

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,5 +27,8 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "tokyonight" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = {
+    enabled = true,
+    frequency = 3600 * 24 * 7,
+  },
 })
