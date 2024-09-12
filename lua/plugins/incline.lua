95572ca73e6a96b3d646e3936e97f2b037764ec2
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-09-12 15:17:26 -0400

    update incline

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -1,14 +1,3 @@
-local function filename_and_parent(path, sep)
-  local segments = vim.split(path, sep)
-  if #segments == 0 then
-    return path
-  elseif #segments == 1 then
-    return segments[#segments]
-  else
-    return table.concat({ segments[#segments - 1], segments[#segments] }, sep)
-  end
-end
-
 return {
   "b0o/incline.nvim",
   enabled = true,
@@ -21,8 +10,10 @@ return {
       },
       render = function(props)
         local bufname = vim.api.nvim_buf_get_name(props.buf)
-        -- local filename = bufname ~= "" and filename_and_parent(vim.fn.fnamemodify(bufname, "%:p:~"), "/") or "[No Name]"
-        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+        local filename = vim.fn.fnamemodify(bufname, ":t") or "[No Name]"
+        if filename == "" then
+          filename = "[No Name]"
+        end
         local icon, color = require("nvim-web-devicons").get_icon_color(filename)
         if vim.api.nvim_get_option_value("readonly", { buf = props.buf }) then
           filename = filename .. " [-]"
@@ -30,24 +21,7 @@ return {
         if vim.api.nvim_get_option_value("modified", { buf = props.buf }) then
           filename = filename .. " [+]"
         end
-
-        local function get_diagnostic_label()
-          local icons = { error = " ", warn = " ", info = " ", hint = " " }
-          local label = {}
-
-          for severity, icon in pairs(icons) do
-            local n = #vim.diagnostic.count(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
-            if n > 0 then
-              table.insert(label, { icon .. n .. " ", group = "DiagnosticSign" .. severity })
-            end
-          end
-          -- if #label > 0 then
-          --   table.insert(label, { "┊ " })
-          -- end
-          return label
-        end
         return {
-          -- { get_diagnostic_label() },
           { icon, guifg = color },
           { " " },
           { filename },
@@ -59,6 +33,10 @@ return {
         },
         zindex = 1,
       },
+      ignore = {
+        filetypes = { "oil" },
+      },
     })
   end,
+  event = "VeryLazy",
 }
