27f4212eea3e034ba5b50f540cab5c8a10e2391c
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2024-09-16 02:13:53 -0400

    update nvim-lspconfig

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -5,30 +5,4 @@ return {
     "folke/neodev.nvim",
   },
   -- requires = { "editorconfig/editorconfig-vim" },
-  config = function()
-    -- require("lspconfig")
-
-    -- local function filter_client(list, val)
-    --   for _, value in ipairs(list) do
-    --     if value == val then
-    --       return false
-    --     end
-    --   end
-    --   return true
-    -- end
-
-    -- vim.lsp.buf.format({
-    --   filter = function(client)
-    --     return filter_client({
-    --       "tsserver",
-    --       "angularls",
-    --       "html",
-    --       "cssls",
-    --       "jsonls",
-    --       "yamls",
-    --       "tailwindcss",
-    --     }, client.name)
-    --   end,
-    -- })
-  end,
 }
