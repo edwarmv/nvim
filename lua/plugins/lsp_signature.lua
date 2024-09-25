7b41a79de771a4b0f5660ff032a2ce609b3886ac
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-09-25 11:50:46 -0400

    disable lsp_signature

--- lua/plugins/lsp_signature.lua
+++ lua/plugins/lsp_signature.lua
@@ -2,11 +2,11 @@ local defaults = require("config.defaults")
 
 return {
   "ray-x/lsp_signature.nvim",
-  enabled = true,
+  enabled = false,
   event = "VeryLazy",
   opts = {
-    max_height = math.floor(vim.o.lines * 0.3), -- max height of signature floating_window
-    max_width = math.floor(vim.o.columns * 0.4), -- max_width of signature floating_window
+    -- max_height = math.floor(vim.o.lines * 0.3), -- max height of signature floating_window
+    -- max_width = math.floor(vim.o.columns * 0.4), -- max_width of signature floating_window
     floating_window = false, -- show hint in a floating window, set to false for virtual text only mode
     hint_enable = true, -- virtual hint enable
     hint_prefix = {
@@ -23,6 +23,16 @@ return {
     move_cursor_key = "<m-p>",
   },
   config = function(_, opts)
-    require("lsp_signature").setup(opts)
+    -- require("lsp_signature").setup(opts)
+    local enabled_severs = { "ts_ls", "angularls", "basedpyright" }
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(args)
+        local bufnr = args.buf
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if vim.tbl_contains(enabled_severs, client.name) then -- blacklist lsp
+          require("lsp_signature").on_attach(opts, bufnr)
+        end
+      end,
+    })
   end,
 }
