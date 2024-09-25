6f6318a695f84055ee2ce8bae8d1c51e6126c8f7
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-09-25 09:07:58 -0400

    update incline

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -1,42 +1,40 @@
 return {
   "b0o/incline.nvim",
   enabled = true,
-  config = function()
-    require("incline").setup({
-      hide = {
-        cursorline = "focused_win",
-        focused_win = true,
-        only_win = false,
+  opts = {
+    hide = {
+      cursorline = "focused_win",
+      focused_win = false,
+      only_win = false,
+    },
+    render = function(props)
+      local bufname = vim.api.nvim_buf_get_name(props.buf)
+      local filename = vim.fn.fnamemodify(bufname, ":t") or "[No Name]"
+      if filename == "" then
+        filename = "[No Name]"
+      end
+      local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+      if vim.api.nvim_get_option_value("readonly", { buf = props.buf }) then
+        filename = filename .. " [-]"
+      end
+      if vim.api.nvim_get_option_value("modified", { buf = props.buf }) then
+        filename = filename .. " [+]"
+      end
+      return {
+        { icon, guifg = color },
+        { " " },
+        { filename },
+      }
+    end,
+    window = {
+      margin = {
+        horizontal = 0,
       },
-      render = function(props)
-        local bufname = vim.api.nvim_buf_get_name(props.buf)
-        local filename = vim.fn.fnamemodify(bufname, ":t") or "[No Name]"
-        if filename == "" then
-          filename = "[No Name]"
-        end
-        local icon, color = require("nvim-web-devicons").get_icon_color(filename)
-        if vim.api.nvim_get_option_value("readonly", { buf = props.buf }) then
-          filename = filename .. " [-]"
-        end
-        if vim.api.nvim_get_option_value("modified", { buf = props.buf }) then
-          filename = filename .. " [+]"
-        end
-        return {
-          { icon, guifg = color },
-          { " " },
-          { filename },
-        }
-      end,
-      window = {
-        margin = {
-          horizontal = 0,
-        },
-        zindex = 1,
-      },
-      ignore = {
-        filetypes = { "oil" },
-      },
-    })
-  end,
+      zindex = 1,
+    },
+    ignore = {
+      filetypes = { "oil" },
+    },
+  },
   event = "VeryLazy",
 }
