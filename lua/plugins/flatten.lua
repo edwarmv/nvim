217e77cf3b7284a7d783bc04a5674e83314bf9f9
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-09-27 14:12:25 -0400

    update flatten

--- lua/plugins/flatten.lua
+++ lua/plugins/flatten.lua
@@ -1,10 +1,11 @@
 return {
   {
     "willothy/flatten.nvim",
-    config = true,
-    -- or pass configuration with
-    -- opts = {  }
-    -- Ensure that it runs first to minimize delay when opening file from terminal
+    opts = {
+      window = {
+        open = "smart",
+      },
+    },
     lazy = false,
     priority = 1001,
   },
