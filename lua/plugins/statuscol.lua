f046b8505b91c30945e7d40007a8632abbcd0540
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-09-30 14:46:12 -0400

    update statuscol

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local builtin = require("statuscol.builtin")
     require("statuscol").setup({
-      ft_ignore = { "NvimTree", "Neogit*", "trouble", "neo-tree", "aerial", "diff", "undotree" },
+      ft_ignore = { "NvimTree", "Neogit*", "trouble", "neo-tree", "aerial", "diff", "undotree", "qf", "quickfix" },
       segments = {
         -- {
         --   sign = { name = { "todo*" }, maxwidth = 1, colwidth = 2, auto = true },
