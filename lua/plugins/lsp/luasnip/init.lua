ba40030ec00ad5068e1b25b9b1c7ab5ea2fb25ae
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2024-10-08 23:04:35 -0400

    update luasnip

--- lua/plugins/lsp/luasnip/init.lua
+++ lua/plugins/lsp/luasnip/init.lua
@@ -32,15 +32,13 @@ return {
         },
       },
     })
-    local ls = require("luasnip")
 
+    -- https://github.com/L3MON4D3/LuaSnip/issues/258
     vim.api.nvim_create_autocmd("ModeChanged", {
+      pattern = { "s:n", "i:*" },
       callback = function()
-        if
-          ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
-          and ls.session.current_nodes[vim.api.nvim_get_current_buf()]
-          and not ls.session.jump_active
-        then
+        local ls = require("luasnip")
+        if ls.session.current_nodes[vim.api.nvim_get_current_buf()] and not ls.session.jump_active then
           ls.unlink_current()
         end
       end,
