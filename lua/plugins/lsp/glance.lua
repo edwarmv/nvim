9da6e55897aa32431ed15f021810599140dfa32e
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-10-09 13:52:04 -0400

    update maps glance

--- lua/plugins/lsp/glance.lua
+++ lua/plugins/lsp/glance.lua
@@ -1,15 +1,30 @@
 return {
   "dnlhc/glance.nvim",
-  opts = {
-    preview_win_opts = {
-      wrap = false,
-    },
-    folds = {
-      fold_closed = "▸",
-      fold_open = "▾",
-    },
-    indent_lines = {
-      enable = false,
-    },
-  },
+  config = function()
+    local glance = require("glance")
+    local actions = glance.actions
+
+    glance.setup({
+      preview_win_opts = {
+        wrap = false,
+      },
+      folds = {
+        fold_closed = "▸",
+        fold_open = "▾",
+      },
+      indent_lines = {
+        enable = false,
+      },
+      mappings = {
+        list = {
+          ["v"] = false,
+          ["s"] = false,
+          ["t"] = false,
+          ["<c-v>"] = actions.jump_vsplit,
+          ["<c-s>"] = actions.jump_split,
+          ["<c-t>"] = actions.jump_tab,
+        },
+      },
+    })
+  end,
 }
