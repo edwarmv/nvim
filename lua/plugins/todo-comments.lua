fcbd6c45ad6af3d13cc0606ff3f8a6ee9ecf8437
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-10-23 14:19:28 -0400

    update todo-comments

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -6,5 +6,19 @@ return {
   keys = {
     { "<space>ft", "<cmd>TodoFzfLua<cr>" },
     { "<leader>xt", "<cmd>TodoTrouble<cr>" },
+    {
+      "]t",
+      function()
+        require("todo-comments").jump_next()
+      end,
+      desc = "Next todo comment",
+    },
+    {
+      "[t",
+      function()
+        require("todo-comments").jump_prev()
+      end,
+      desc = "Previous todo comment",
+    },
   },
 }
