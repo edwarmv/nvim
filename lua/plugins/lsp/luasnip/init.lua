b4d1897035888456e878d814489194e8373f0416
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-10-24 16:10:29 -0400

    update luasnip

--- lua/plugins/lsp/luasnip/init.lua
+++ lua/plugins/lsp/luasnip/init.lua
@@ -14,9 +14,16 @@ return {
     local types = require("luasnip.util.types")
     require("plugins.lsp.luasnip.snippets")
     luasnip.setup({
-      delete_check_events = "TextChanged",
+      delete_check_events = { "TextChanged", "InsertLeave" },
+      region_check_events = { "CursorMoved", "CursorHold", "InsertEnter" },
       ext_opts = {
         [types.insertNode] = {
+          unvisited = {
+            virt_text = { { "$", "Conceal" } },
+            virt_text_pos = "inline",
+          },
+        },
+        [types.choiceNode] = {
           unvisited = {
             virt_text = { { "$", "Conceal" } },
             virt_text_pos = "inline",
@@ -33,20 +40,20 @@ return {
       },
     })
 
-    vim.api.nvim_create_autocmd("ModeChanged", {
-      group = vim.api.nvim_create_augroup("mariasolos/unlink_snippet", { clear = true }),
-      desc = "Cancel the snippet session when leaving insert mode",
-      pattern = { "s:n", "i:*" },
-      callback = function(args)
-        if
-          luasnip.session
-          and luasnip.session.current_nodes[args.buf]
-          and not luasnip.session.jump_active
-          -- and not luasnip.choice_active()
-        then
-          luasnip.unlink_current()
-        end
-      end,
-    })
+    -- vim.api.nvim_create_autocmd("ModeChanged", {
+    --   group = vim.api.nvim_create_augroup("mariasolos/unlink_snippet", { clear = true }),
+    --   desc = "Cancel the snippet session when leaving insert mode",
+    --   pattern = { "s:n", "i:*" },
+    --   callback = function(args)
+    --     if
+    --       luasnip.session
+    --       and luasnip.session.current_nodes[args.buf]
+    --       and not luasnip.session.jump_active
+    --       -- and not luasnip.choice_active()
+    --     then
+    --       luasnip.unlink_current()
+    --     end
+    --   end,
+    -- })
   end,
 }
