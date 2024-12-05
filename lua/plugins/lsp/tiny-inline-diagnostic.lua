3cda07506d7bb9ff9f51d41c22f09b8345382077
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-12-05 16:53:49 -0400

    update tiny inline diagnostic

--- lua/plugins/lsp/tiny-inline-diagnostic.lua
+++ lua/plugins/lsp/tiny-inline-diagnostic.lua
@@ -7,4 +7,18 @@ return {
     preset = "classic",
     throttle = defaults.lsp_debounce,
   },
+  config = function(_, opts)
+    require("tiny-inline-diagnostic").setup(opts)
+
+    vim.api.nvim_create_autocmd("InsertEnter", {
+      callback = function()
+        require("tiny-inline-diagnostic").disable()
+      end,
+    })
+    vim.api.nvim_create_autocmd("InsertLeave", {
+      callback = function()
+        require("tiny-inline-diagnostic").enable()
+      end,
+    })
+  end,
 }
