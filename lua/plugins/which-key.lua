7b3face4b53380f29035b7f3382fa11ce8bfe366
Edwar Martinez Vale <edwar.martinez@hotmail.com>
2025-01-17 13:41:50 -0400

    update which-key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,7 +2,7 @@ return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   opts = {
-    preset = "classic",
+    preset = "helix",
     delay = function(ctx)
       return ctx.plugin and 0 or 500
     end,
