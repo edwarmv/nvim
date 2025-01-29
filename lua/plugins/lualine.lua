15856e9a144341d3eddb7ad4eb72f96c73016f06
Edwar Martinez Vale <edwar.martinez@hotmail.com>
2025-01-29 13:25:21 -0400

    remove noice

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -16,7 +16,6 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = {
     "nvim-tree/nvim-web-devicons",
-    "folke/noice.nvim",
     "letieu/harpoon-lualine",
   },
   config = function()
@@ -28,25 +27,6 @@ return {
         component_separators = { left = "", right = "" },
         disabled_filetypes = {
           statusline = { "dashboard" },
-          winbar = {
-            "neo-tree",
-            "dashboard",
-            "packer",
-            "NvimTree",
-            "aerial",
-            "trouble",
-            "noice",
-            "lazy",
-            "mason",
-            "qf",
-            "DiffviewFileHistory",
-            "toggleterm",
-            "NeogitStatus",
-            "NeogitPopup",
-            "NeogitDiffView",
-            "undotree",
-            "diff",
-          },
         },
         globalstatus = true,
         always_divide_middle = true,
@@ -85,12 +65,6 @@ return {
           },
         },
         lualine_x = {
-          {
-            require("noice").api.status.mode.get,
-            cond = require("noice").api.status.mode.has,
-            color = { fg = "#ff9e64" },
-            padding = { left = 0, right = 1 },
-          },
           { "searchcount", padding = { left = 0, right = 1 } },
           { "selectioncount", padding = { left = 0, right = 1 } },
           { "filetype", padding = { left = 0, right = 1 } },
@@ -107,46 +81,6 @@ return {
         lualine_y = { "progress" },
         lualine_z = { "location" },
       },
-      -- winbar = {
-      --   lualine_c = {
-      --     { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-      --     { "filename", file_status = true, separator = "", padding = 0 },
-      --     { "diff", source = diff_source, padding = { left = 1, right = 0 } },
-      --     { conflict_count, color = { fg = "#b2555b" }, padding = { left = 1, right = 0 } },
-      --     {
-      --       "diagnostics",
-      --       sources = { "nvim_diagnostic" }, -- coc nvim_diagnostic
-      --       symbols = {
-      --         error = icons.diagnostics.error,
-      --         warn = icons.diagnostics.warn,
-      --         info = icons.diagnostics.info,
-      --         hint = icons.diagnostics.hint,
-      --       },
-      --       update_in_insert = false, -- Update diagnostics in insert mode
-      --       padding = { left = 1, right = 0 },
-      --     },
-      --   },
-      -- },
-      -- inactive_winbar = {
-      --   lualine_c = {
-      --     { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-      --     { "filename", file_status = true, separator = "", padding = 0 },
-      --     { "diff", source = diff_source, padding = { left = 1, right = 0 } },
-      --     { conflict_count, color = { fg = "#b2555b" }, padding = { left = 1, right = 0 } },
-      --     {
-      --       "diagnostics",
-      --       sources = { "nvim_diagnostic" }, -- coc nvim_diagnostic
-      --       symbols = {
-      --         error = icons.diagnostics.error,
-      --         warn = icons.diagnostics.warn,
-      --         info = icons.diagnostics.info,
-      --         hint = icons.diagnostics.hint,
-      --       },
-      --       update_in_insert = false, -- Update diagnostics in insert mode
-      --       padding = { left = 1, right = 0 },
-      --     },
-      --   },
-      -- },
       extensions = {
         "aerial",
         "chadtree",
