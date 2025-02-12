4b4ee1c891389c01ac4d5c225f552308bfcea341
Edwar Martinez Vale <edwar.martinez@hotmail.com>
2025-02-12 16:06:57 -0400

    update nvim cmp

--- lua/plugins/lsp/nvim-cmp.lua
+++ lua/plugins/lsp/nvim-cmp.lua
@@ -9,7 +9,6 @@ end
 return {
   "hrsh7th/nvim-cmp",
   enabled = true,
-  name = "nvim-cmp",
   event = { "InsertEnter", "CmdlineEnter" },
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
@@ -42,138 +41,74 @@ return {
           require("luasnip").lsp_expand(args.body)
         end,
       },
-      preselect = cmp.PreselectMode.Item, -- None - Item
       window = {
         completion = {
-          -- border = defaults.border,
-          winhighlight = "Normal:Pmenu,FloatBorder:CmpBorder,CursorLine:PmenuSel,Search:None",
           col_offset = -2,
-        },
-        documentation = {
-          max_width = math.floor(vim.opt.columns:get() / 3),
-          max_height = math.floor(vim.opt.lines:get() / 3),
-          -- border = { "🭽", "▔", "🭾", "▕", "🭿", "▁", "🭼", "▏" },
-          winhighlight = "FloatBorder:CmpBorder,Search:None",
         },
       },
       view = {
         entries = {
+          name = "custom",
           follow_cursor = true,
-        },
-        docs = {
-          auto_open = true,
         },
       },
       mapping = {
-        ["<up>"] = cmp.mapping({
-          i = function(fallback)
-            if cmp.visible() then
-              cmp.close()
-              fallback()
-            else
-              fallback()
-            end
-          end,
-        }),
-        ["<down>"] = cmp.mapping({
-          i = function(fallback)
-            if cmp.visible() then
-              cmp.close()
-              fallback()
-            else
-              fallback()
-            end
-          end,
-        }),
-        ["<c-n>"] = cmp.mapping({
-          i = function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-            else
-              fallback()
-            end
-          end,
-        }),
-        ["<c-p>"] = cmp.mapping({
-          i = function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-            else
-              fallback()
-            end
-          end,
-        }),
-        ["<c-f>"] = cmp.mapping.scroll_docs(1),
-        ["<c-b>"] = cmp.mapping.scroll_docs(-1),
-        ["<c-e>"] = cmp.mapping({
-          i = function(fallback)
-            -- if luasnip.choice_active() then
-            --   luasnip.change_choice(1)
-            -- end
-            if cmp.visible() then
-              cmp.close()
-            else
-              fallback()
-            end
-          end,
-          -- s = function(fallback)
-          --   if luasnip.choice_active() then
-          --     luasnip.change_choice(1)
-          --   else
-          --     fallback()
-          --   end
-          -- end,
-        }),
+        ["<c-n>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+          else
+            fallback()
+          end
+        end),
+        ["<c-p>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+          else
+            fallback()
+          end
+        end),
+        ["<c-f>"] = cmp.mapping.scroll_docs(4),
+        ["<c-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<c-e>"] = cmp.mapping(function(fallback)
+          -- if luasnip.choice_active() then
+          --   luasnip.change_choice(1)
+          -- end
+          if cmp.visible() then
+            cmp.close()
+          else
+            fallback()
+          end
+        end),
         ["<c-y>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }),
         ["<CR>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
-        ["<tab>"] = cmp.mapping({
-          i = function(fallback)
-            if cmp.visible() then
-              cmp.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace })
-            elseif luasnip.locally_jumpable(1) then
-              luasnip.jump(1)
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end,
-          s = function(fallback)
-            if luasnip.jumpable(1) then
-              luasnip.jump(1)
-            else
-              fallback()
-            end
-          end,
-        }),
-        ["<s-tab>"] = cmp.mapping({
-          i = function(fallback)
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end,
-          s = function(fallback)
-            if luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end,
-        }),
-        ["<c-space>"] = cmp.mapping({
-          i = function()
+        ["<tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace })
+          elseif luasnip.locally_jumpable(1) then
+            luasnip.jump(1)
+          elseif has_words_before() then
             cmp.complete()
-            if cmp.get_selected_entry() ~= nil then
-              if cmp.visible_docs() then
-                cmp.close_docs()
-              else
-                cmp.open_docs()
-              end
+          else
+            fallback()
+          end
+        end),
+        ["<s-tab>"] = cmp.mapping(function(fallback)
+          if luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end),
+        ["<c-space>"] = cmp.mapping(function()
+          cmp.complete()
+          if cmp.get_selected_entry() ~= nil then
+            if cmp.visible_docs() then
+              cmp.close_docs()
+            else
+              cmp.open_docs()
             end
-          end,
-        }),
+          end
+        end),
       },
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
@@ -182,6 +117,7 @@ return {
         { name = "buffer" },
       }),
       formatting = {
+        expandable_indicator = true,
         fields = {
           "kind",
           "abbr",
@@ -193,7 +129,7 @@ return {
             return math.floor(vim.o.columns / 5)
           end,
           show_labelDetails = true,
-          ellipsis_char = "… ",
+          ellipsis_char = "…",
           before = require("tailwind-tools.cmp").lspkind_format,
         }),
       },
@@ -209,20 +145,48 @@ return {
           col_offset = 0,
         },
       },
+      view = {
+        entries = {
+          name = "custom",
+          follow_cursor = false,
+        },
+      },
       sources = cmp.config.sources({
         { name = "path" },
       }, {
         { name = "cmdline" },
       }),
-      matching = { disallow_symbol_nonprefix_matching = false },
+      matching = {
+        disallow_fuzzy_matching = false,
+        disallow_fullfuzzy_matching = false,
+        disallow_partial_fuzzy_matching = true,
+        disallow_partial_matching = false,
+        disallow_prefix_unmatching = false,
+        disallow_symbol_nonprefix_matching = false,
+      },
       formatting = {
+        expandable_indicator = true,
         fields = {
-          cmp.ItemField.Abbr,
+          "abbr",
         },
+        format = function(_, vim_item)
+          return vim_item
+        end,
       },
     })
 
     cmp.setup.cmdline({ "/", "?" }, {
+      window = {
+        completion = {
+          col_offset = -2,
+        },
+      },
+      view = {
+        entries = {
+          name = "custom",
+          follow_cursor = false,
+        },
+      },
       mapping = cmp.mapping.preset.cmdline(),
       sources = cmp.config.sources({
         { name = "nvim_lsp_document_symbol" },
