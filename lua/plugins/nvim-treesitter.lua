a9e3c8adabf58f1153c9d4816383d4e6ef89eee1
Edwar Martinez Vale <edwar.martinez@hotmail.com>
2025-02-12 16:06:09 -0400

    update nvim-treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -45,12 +45,12 @@ return {
       "rust",
       "xml",
     },
-    additional_vim_regex_highlighting = { "scss" },
+    additional_vim_regex_highlighting = {},
     highlight = {
       enable = true,
       disable = function(lang, buf)
         local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
         if ok and stats and stats.size > max_filesize then
           return true
         end
