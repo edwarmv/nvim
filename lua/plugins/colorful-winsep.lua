b01646e2aafc9bf5ddd2a0947323044f4d27d91b
Edwar Martinez Vale <edwar.martinez@hotmail.com>
2025-02-13 17:12:31 -0400

    add colorful-winsep

---
+++ lua/plugins/colorful-winsep.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-zh/colorful-winsep.nvim",
+  enabled = true,
+  opts = {
+    highlight = {
+      fg = "#29a4bd",
+      bg = "#24283b",
+    },
+  },
+  event = { "WinNew" },
+}
