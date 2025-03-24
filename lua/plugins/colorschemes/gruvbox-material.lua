5cfe0e48f590580afbd0cf3d36ccf78db0ef514c
Edwar Martinez Vale <edwar.martinez@hotmail.com>
2025-03-24 08:13:07 -0400

    update gruvbox-material

--- lua/plugins/colorschemes/gruvbox-material.lua
+++ lua/plugins/colorschemes/gruvbox-material.lua
@@ -1,7 +1,23 @@
 return {
   "sainnhe/gruvbox-material",
-  config = function()
-    -- local utils = require("utils")
-    -- utils.sourceVim("gruvbox-material.vim")
+  lazy = false,
+  priority = 1000,
+  init = function()
+    vim.g.gruvbox_material_enable_italic = true
+    vim.g.gruvbox_material_better_performance = true
+    vim.g.gruvbox_material_enable_bold = true
+
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      group = vim.api.nvim_create_augroup("custom_highlights_gruvboxmaterial", {}),
+      pattern = "gruvbox-material",
+      callback = function()
+        local config = vim.fn["gruvbox_material#get_configuration"]()
+        local palette =
+          vim.fn["gruvbox_material#get_palette"](config.background, config.foreground, config.colors_override)
+        local set_hl = vim.fn["gruvbox_material#highlight"]
+
+        set_hl("ExtraWhitespace", palette.red, palette.bg_visual_red)
+      end,
+    })
   end,
 }
