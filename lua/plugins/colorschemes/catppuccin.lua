161dffd6e01316f5b2207948d25f7503249c1a4f
Edwar Martinez Vale <edwar.martinez@hotmail.com>
2025-03-26 13:07:59 -0400

    enable catppuccin colorscheme

--- lua/plugins/colorschemes/catppuccin.lua
+++ lua/plugins/colorschemes/catppuccin.lua
@@ -1,40 +1,39 @@
 return {
   "catppuccin/nvim",
-  enabled = false,
   name = "catppuccin",
   priority = 1000,
-  config = function()
-    require("catppuccin").setup({
-      dim_inactive = {
-        enabled = true, -- dims the background color of inactive window
-        shade = "dark",
-        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+  opts = {
+    dim_inactive = {
+      enabled = true, -- dims the background color of inactive window
+      shade = "dark",
+      percentage = 0.15, -- percentage of the shade to apply to the inactive window
+    },
+    flavour = "macchiato",
+    integrations = {
+      mason = true,
+      flash = true,
+      gitsigns = true,
+      hop = true,
+      neotree = true,
+      noice = true,
+      cmp = true,
+      navic = {
+        enabled = true,
+        custom_bg = "NONE", -- "lualine" will set background to mantle
       },
-      flavour = "macchiato",
-      integrations = {
-        mason = true,
-        flash = true,
-        gitsigns = true,
-        hop = true,
-        neotree = true,
-        noice = true,
-        cmp = true,
-        navic = {
-          enabled = true,
-          custom_bg = "NONE", -- "lualine" will set background to mantle
-        },
-        notify = true,
-        semantic_tokens = true,
-        treesitter = true,
-        telescope = {
-          enabled = true,
-          -- style = "nvchad"
-        },
-        lsp_trouble = true,
-        vimwiki = true,
-        which_key = true,
+      notify = true,
+      semantic_tokens = true,
+      treesitter = true,
+      telescope = {
+        enabled = true,
+        -- style = "nvchad"
       },
-    })
-    vim.cmd.colorscheme("catppuccin")
+      lsp_trouble = true,
+      vimwiki = true,
+      which_key = true,
+    },
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
   end,
 }
