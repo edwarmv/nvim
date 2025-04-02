72cbb61642784403f7292458141b27852df99967
Edwar Martinez Vale <edwar.martinez@hotmail.com>
2025-04-02 13:01:48 -0400

    update markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,14 +1,26 @@
 return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  opts = {},
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "echasnovski/mini.icons",
-    {
-      "iamcco/markdown-preview.nvim",
-      build = function()
-        vim.fn["mkdp#util#install"]()
-      end,
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      overrides = {
+        buftype = {
+          nofile = {
+            code = {
+              style = "normal",
+            },
+          },
+        },
+      },
     },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "echasnovski/mini.icons",
+    },
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
   },
 }
