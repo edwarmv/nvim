0a983b061905c76c4ec90021fd6a595d8ec7e740
Edwar Martinez Vale <edwar.martinez@hotmail.com>
2025-04-07 17:26:38 -0400

    update blink

--- lua/plugins/lsp/blink.cmp.lua
+++ lua/plugins/lsp/blink.cmp.lua
@@ -63,8 +63,6 @@ return {
     },
     completion = {
       documentation = {
-        auto_show = true,
-        auto_show_delay_ms = 200,
         window = {
           border = "padded",
         },
