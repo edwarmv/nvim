d179cca73ffab64877a1a75a8d1ff335d8fe159b
Edwar Martinez Vale <edwar.martinez@hotmail.com>
2025-04-08 16:29:14 -0400

    update tiny-inline-diagnostics

--- lua/plugins/lsp/tiny-inline-diagnostic.lua
+++ lua/plugins/lsp/tiny-inline-diagnostic.lua
@@ -1,7 +1,13 @@
+local defaults = require("config.defaults")
+
 return {
   "rachartier/tiny-inline-diagnostic.nvim",
   event = "VimEnter",
   opts = {
     preset = "simple",
+    options = {
+      throttle = defaults.lsp_debounce,
+      use_icons_from_diagnostic = true,
+    },
   },
 }
