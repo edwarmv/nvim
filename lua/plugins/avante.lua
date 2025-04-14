8f8a8313715eafcbe5fc0c7adb30e2a136786986
Edwar Martinez Vale <edwar.martinez@hotmail.com>
2025-04-14 14:19:20 -0400

    update avante

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -15,20 +15,6 @@ return {
     "nvim-cmp", -- autocompletion for avante commands and mentions
     "echasnovski/mini.icons",
     "zbirenbaum/copilot.lua", -- for providers='copilot'
-    {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-        },
-      },
-    },
     "MeanderingProgrammer/render-markdown.nvim",
   },
 }
