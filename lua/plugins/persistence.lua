2fef10a97f306a09b0ebd21664db02ebd6598aed
Edwar Martinez Vale <edwar.martinez@hotmail.com>
2025-04-25 14:51:59 -0400

    enable persistence

--- lua/plugins/persistence.lua
+++ lua/plugins/persistence.lua
@@ -1,6 +1,5 @@
 return {
   "folke/persistence.nvim",
-  enabled = false,
   event = "BufReadPre", -- this will only start session saving when an actual file was opened
   opts = {
     -- add any custom options here
