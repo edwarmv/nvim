4516cb042869d6a50196c4fc3569e2da2171acab
Edwar Martinez Vale <edwar.martinez@hotmail.com>
2025-05-08 15:57:18 -0400

    update mason

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -12,13 +12,57 @@ return {
     },
   },
   {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = {
+      ensure_installed = {
+        -- formatters
+        "black",
+        "sql-formatter",
+        -- "prettier",
+        "prettierd",
+        "stylua",
+        "autopep8",
+        "eslint-lsp",
+        "eslint_d",
+        -- "ruff",
+        -- DAP
+        "js-debug-adapter",
+      },
+      integrations = {
+        ["mason-lspconfig"] = false,
+        ["mason-null-ls"] = true,
+        ["mason-nvim-dap"] = true,
+      },
+    },
+  },
+  {
     "williamboman/mason-lspconfig.nvim",
     dependencies = {
       "neovim/nvim-lspconfig",
       "williamboman/mason.nvim",
     },
     opts = {
-      ensure_installed = {},
+      ensure_installed = {
+        "bashls",
+        "ts_ls",
+        -- "vtsls",
+        "angularls",
+        "html",
+        "cssls",
+        "vimls",
+        "yamlls",
+        "jsonls",
+        "lua_ls",
+        "taplo",
+        "tailwindcss",
+        "emmet_language_server",
+        "astro",
+        "basedpyright",
+        "css_variables",
+        "dockerls",
+        "docker_compose_language_service",
+        "solidity_ls_nomicfoundation",
+      },
     },
   },
 }
