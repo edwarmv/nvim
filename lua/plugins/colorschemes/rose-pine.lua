262af0bf0e65914f94330c01fdc1638963a1012f
Edwar Martinez Vale <edwar.martinez@hotmail.com>
2025-05-19 16:17:35 -0400

    update rose-pine

--- lua/plugins/colorschemes/rose-pine.lua
+++ lua/plugins/colorschemes/rose-pine.lua
@@ -1,5 +1,9 @@
 return {
   "rose-pine/neovim",
   name = "rose-pine",
-  opts = {},
+  opts = {
+    highlight_groups = {
+      ExtraWhitespace = { fg = "love", bg = "love", blend = 20 },
+    },
+  },
 }
