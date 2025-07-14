179d105e49529296cd0066cbedca5e6b0c4e4ded
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-07-13 20:01:18 -0400

    update nvim-window-picker

--- lua/plugins/nvim-window-picker.lua
+++ lua/plugins/nvim-window-picker.lua
@@ -5,55 +5,6 @@ return {
   opts = {
     hint = "floating-letter",
     show_prompt = false,
-    filter_rules = {
-      autoselect_one = true,
-      include_current_win = false,
-      bo = {
-        filetype = {
-          "NvimTree",
-          "neo-tree",
-          "neo-tree-popup",
-          "notify",
-          "incline",
-          "fidget",
-          "packer",
-          "qf",
-          "diff",
-          "fugitive",
-          "fugitiveblame",
-        },
-        buftype = { "terminal", "nofile", "terminal", "help" },
-      },
-      wo = {},
-      file_path_contains = {},
-      file_name_contains = {},
-    },
-    highlights = {
-      statusline = {
-        focused = {
-          fg = "#a9b1d6",
-          bg = "#1f2335",
-          bold = true,
-        },
-        unfocused = {
-          fg = "#a9b1d6",
-          bg = "#1f2335",
-          bold = true,
-        },
-      },
-      winbar = {
-        focused = {
-          fg = "#a9b1d6",
-          bg = "#1f2335",
-          bold = true,
-        },
-        unfocused = {
-          fg = "#a9b1d6",
-          bg = "#1f2335",
-          bold = true,
-        },
-      },
-    },
   },
   keys = {
     {
