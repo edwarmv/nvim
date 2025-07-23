22b427b9ca9f64a9cd13429455ca04058bf5ca88
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-07-23 11:07:57 -0400

    update avante

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -9,13 +9,7 @@ return {
   },
   build = "make",
   dependencies = {
-    "nvim-treesitter/nvim-treesitter",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
-    "nvim-cmp", -- autocompletion for avante commands and mentions
-    "echasnovski/mini.icons",
-    -- "zbirenbaum/copilot.lua",
-    "MeanderingProgrammer/render-markdown.nvim",
   },
 }
