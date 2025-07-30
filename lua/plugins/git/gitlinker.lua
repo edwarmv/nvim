1bcbf486fafd1985178082fbdd114d0950d1a740
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-07-30 10:20:13 -0400

    update gitlinker

--- lua/plugins/git/gitlinker.lua
+++ lua/plugins/git/gitlinker.lua
@@ -1,7 +1,19 @@
 return {
   "linrongbin16/gitlinker.nvim",
   cmd = "GitLink",
-  opts = {},
+  opts = function()
+    local routers = require("gitlinker.routers")
+    return {
+      router = {
+        browse = {
+          ["^gitlab%.ekupd%.com"] = routers.gitlab_browse,
+        },
+        blame = {
+          ["^gitlab%.ekupd%.com"] = routers.gitlab_blame,
+        },
+      },
+    }
+  end,
   keys = {
     { "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
     { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
