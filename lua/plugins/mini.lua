08d6451f7760db4a757bd72c4fe8f325467b3b9b
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-07-30 10:25:06 -0400

    remove luasnip and mini snip

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -16,54 +16,6 @@ return {
     end,
   },
   {
-    "echasnovski/mini.snippets",
-    enabled = false,
-    dependencies = {
-      "rafamadriz/friendly-snippets",
-    },
-    config = function()
-      local gen_loader = require("mini.snippets").gen_loader
-
-      local javascript = "**/javascript.json"
-      local react = "**/react.json"
-      local lang_patterns = {
-        typescript = { javascript },
-        astro = { javascript },
-        tsx = { react, javascript },
-      }
-      require("mini.snippets").setup({
-        snippets = {
-          -- -- Load custom file with global snippets first (adjust for Windows)
-          -- gen_loader.from_file('~/.config/nvim/snippets/global.json'),
-
-          -- Load snippets based on current language by reading files from
-          -- "snippets/" subdirectories from 'runtimepath' directories.
-          gen_loader.from_lang({
-            lang_patterns = lang_patterns,
-          }),
-        },
-        mappings = {
-          expand = "",
-          jump_next = "",
-          jump_prev = "",
-          stop = "<C-c>",
-        },
-      })
-      -- MiniSnippets.start_lsp_server()
-      local make_stop = function()
-        local au_opts = { pattern = "*:n", once = true }
-        au_opts.callback = function()
-          while MiniSnippets.session.get() do
-            MiniSnippets.session.stop()
-          end
-        end
-        vim.api.nvim_create_autocmd("ModeChanged", au_opts)
-      end
-      local opts = { pattern = "MiniSnippetsSessionStart", callback = make_stop }
-      vim.api.nvim_create_autocmd("User", opts)
-    end,
-  },
-  {
     "echasnovski/mini.indentscope",
     init = function()
       vim.api.nvim_create_autocmd("TermOpen", {
