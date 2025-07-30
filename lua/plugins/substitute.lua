7e26905ef6ffe0baa24f3afba75c3582cd9d5981
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-07-30 10:20:56 -0400

    update substitute

--- lua/plugins/substitute.lua
+++ lua/plugins/substitute.lua
@@ -1,22 +1,31 @@
 return {
   "gbprod/substitute.nvim",
-  -- dependencies = { "gbprod/yanky.nvim" },
-  config = function()
-    require("substitute").setup({
-      -- on_substitute = require("yanky.integration").substitute(),
-    })
-
-    vim.keymap.set("n", "cx", function()
-      require("substitute.exchange").operator()
-    end, { noremap = true })
-    vim.keymap.set("n", "cxx", function()
-      require("substitute.exchange").line()
-    end, { noremap = true })
-    vim.keymap.set("x", "X", function()
-      require("substitute.exchange").visual()
-    end, { noremap = true })
-    vim.keymap.set("n", "cxc", function()
-      require("substitute.exchange").cancel()
-    end, { noremap = true })
-  end,
+  opts = {},
+  keys = {
+    {
+      "cx",
+      function()
+        require("substitute.exchange").operator()
+      end,
+    },
+    {
+      "cxx",
+      function()
+        require("substitute.exchange").line()
+      end,
+    },
+    {
+      "X",
+      function()
+        require("substitute.exchange").visual()
+      end,
+      mode = "x",
+    },
+    {
+      "cxc",
+      function()
+        require("substitute.exchange").cancel()
+      end,
+    },
+  },
 }
