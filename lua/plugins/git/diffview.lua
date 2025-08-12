5b7b057c32e6d9d9de5825a5267c013b22acda86
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-08-11 20:25:45 -0400

    update diffview

--- lua/plugins/git/diffview.lua
+++ lua/plugins/git/diffview.lua
@@ -3,9 +3,30 @@ return {
   opts = {},
   cmd = { "DiffviewOpen" },
   keys = {
-    { "<leader><leader>do", "<cmd>DiffviewOpen<cr>", desc = "Diffview - Open" },
-    { "<leader><leader>dc", "<cmd>DiffviewClose<cr>", desc = "Diffview - Close" },
-    { "<leader><leader>dH", "<cmd>DiffviewFileHistory<cr>", desc = "Diffview - File History" },
-    { "<leader><leader>dh", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview - File History Current File" },
+    { "<leader>gdo", "<cmd>DiffviewOpen<cr>", desc = "Diffview - Open" },
+    {
+      "<leader>gdO",
+      function()
+        local diffview = require("diffview")
+        local all = vim.fn.systemlist({ "git", "rev-parse", "--symbolic", "--branches", "--tags", "--remotes" })
+        local original_base = "Original base"
+        table.insert(all, 1, original_base)
+        vim.ui.select(all, {
+          prompt = "Select branch",
+        }, function(choice)
+          if choice == nil then
+            return
+          elseif choice == original_base then
+            choice = nil
+          end
+          diffview.open({ choice })
+        end)
+      end,
+      desc = "Diffview - Open",
+    },
+    { "<leader>gdo", "<cmd>DiffviewOpen<cr>", desc = "Diffview - Open" },
+    { "<leader>gdq", "<cmd>DiffviewClose<cr>", desc = "Diffview - Close" },
+    { "<leader>gdH", "<cmd>DiffviewFileHistory<cr>", desc = "Diffview - File History" },
+    { "<leader>gdh", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview - File History Current File" },
   },
 }
