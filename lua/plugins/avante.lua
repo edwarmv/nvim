ab2bc619241579ab0649f9d692207afae3c2895a
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-08-11 20:25:53 -0400

    update avante

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,10 +1,20 @@
 return {
   "yetone/avante.nvim",
-  enabled = true,
+  enabled = false,
   event = "VeryLazy",
-  version = false, -- Never set this value to "*"! Never!
   opts = {
-    provider = "gemini",
+    provider = "gpt-oss",
+    providers = {
+      ["gpt-oss"] = {
+        __inherited_from = "openai",
+        endpoint = "http://localhost:1234/v1",
+        model = "openai/gpt-oss-20b",
+        extra_request_body = {
+          max_completion_tokens = 4096,
+          max_tokens = 4096,
+        },
+      },
+    },
     hints = { enabled = false },
   },
   build = "make",
