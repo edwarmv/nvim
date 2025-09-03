c9bc0dc8d846ad3b50c73389d2131f9589b90557
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-09-02 23:16:37 -0400

    update goto-preview

--- lua/plugins/lsp/goto-preview.lua
+++ lua/plugins/lsp/goto-preview.lua
@@ -2,10 +2,8 @@ local defaults = require("config.defaults")
 
 return {
   "rmagatti/goto-preview",
-  enabled = true,
-  branch = "fix-lsp-handler-nvim-010",
   dependencies = { "rmagatti/logger.nvim" },
-  event = "BufEnter",
+  event = "LspAttach",
   opts = {
     width = math.floor(vim.o.columns / 3), -- Width of the floating window
     height = math.floor(vim.o.lines / 4), -- Height of the floating window
