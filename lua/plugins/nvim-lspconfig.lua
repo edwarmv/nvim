44b1a0b4ab0813e47704936e5f48794a81ff9487
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-09-08 07:43:10 -0400

    update nvim-lspconfig

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,36 +1,19 @@
 return {
   "neovim/nvim-lspconfig",
   event = "BufReadPre",
-  depependencies = {
-    "saghen/blink.cmp",
-    -- "hrsh7th/nvim-ix",
-    -- "cmp-nvim-lsp",
-  },
   config = function()
-    -- local capabilities = require("cmp_nvim_lsp").default_capabilities()
-    -- capabilities.workspace = {
-    --   didChangeWatchedFiles = {
-    --     dynamicRegistration = true,
-    --   },
-    -- }
     local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = vim.tbl_deep_extend(
-      "force",
-      capabilities,
-      require("blink.cmp").get_lsp_capabilities(),
-      -- require("ix").get_capabilities(),
-      {
-        workspace = {
-          didChangeWatchedFiles = {
-            dynamicRegistration = true,
-          },
-          fileOperations = {
-            didRename = true,
-            willRename = true,
-          },
+    capabilities = vim.tbl_deep_extend("force", capabilities, {
+      workspace = {
+        didChangeWatchedFiles = {
+          dynamicRegistration = true,
         },
-      }
-    )
+        fileOperations = {
+          didRename = true,
+          willRename = true,
+        },
+      },
+    })
     vim.lsp.config("*", {
       capabilities = capabilities,
     })
