8496f3f40fa0efa9e7d639e8f00fc1427f25c8b4
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-09-11 08:50:09 -0400

    update goto-preview

--- lua/plugins/lsp/goto-preview.lua
+++ lua/plugins/lsp/goto-preview.lua
@@ -9,5 +9,11 @@ return {
     height = math.floor(vim.o.lines / 4), -- Height of the floating window
     default_mappings = false, -- Bind default mappings
     border = defaults.border,
+    post_open_hook = function()
+      vim.api.nvim_win_set_config(0, {
+        width = math.floor(vim.o.columns / 3), -- Width of the floating window
+        height = math.floor(vim.o.lines / 4), -- Height of the floating window
+      })
+    end,
   },
 }
