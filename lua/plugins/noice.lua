c95668b975aaa083fb2eca0458b64f5d930e4d9c
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-09-12 11:51:30 -0400

    add noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,17 +1,12 @@
 return {
   "folke/noice.nvim",
-  enabled = true,
   event = "VeryLazy",
   dependencies = {
     "MunifTanjim/nui.nvim",
   },
   opts = {
     messages = {
-      enabled = true,
       view_search = false,
-    },
-    notify = {
-      enabled = false,
     },
     lsp = {
       progress = {
@@ -19,84 +14,13 @@ return {
       },
       hover = {
         enabled = false,
-        silent = true,
       },
       signature = {
         enabled = false,
       },
     },
-    views = {
-      split = {
-        enter = true,
-      },
-      cmdline_popup = {
-        size = {
-          min_width = 0,
-          width = "40%",
-          height = "auto",
-        },
-      },
-      popup = {
-        size = {
-          width = "50%",
-          height = "auto",
-        },
-      },
-      confirm = {},
-      hover = {
-        border = {
-          padding = { 0, 0 },
-        },
-      },
-      -- mini = {
-      --   win_options = {
-      --     winblend = 0,
-      --   },
-      -- },
-    },
-    routes = {
-      -- {
-      --   filter = {
-      --     event = "notify",
-      --     kind = "warn",
-      --   },
-      --   opts = { skip = true },
-      -- },
-      {
-        filter = {
-          event = "msg_show",
-          kind = "lua_error",
-        },
-        opts = { skip = true },
-      },
-      -- {
-      --   filter = {
-      --     event = "msg_show",
-      --     any = {
-      --       { find = "deprecated" },
-      --     },
-      --   },
-      --   opts = { skip = true },
-      -- },
-      -- {
-      --   filter = {
-      --     event = "msg_show",
-      --     kind = "",
-      --   },
-      --   opts = { skip = true },
-      -- },
-      -- {
-      --   filter = {
-      --     event = "notify",
-      --     kind = "warn",
-      --     cond = function(message)
-      --       return message.opts.title == "Neogit"
-      --     end,
-      --   },
-      --   opts = { skip = true },
-      -- },
-    },
     presets = {
+      command_palette = true,
       long_message_to_split = true,
       bottom_search = true,
     },
