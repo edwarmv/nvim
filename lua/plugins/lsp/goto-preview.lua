d1f2547a43ee4445d27bc72bb967d9b962c19489
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-09-12 11:51:21 -0400

    update goto-preview

--- lua/plugins/lsp/goto-preview.lua
+++ lua/plugins/lsp/goto-preview.lua
@@ -5,14 +5,14 @@ return {
   dependencies = { "rmagatti/logger.nvim" },
   event = "LspAttach",
   opts = {
-    width = math.floor(vim.o.columns / 3), -- Width of the floating window
-    height = math.floor(vim.o.lines / 4), -- Height of the floating window
+    width = math.floor(vim.o.columns / 2), -- Width of the floating window
+    height = math.floor(vim.o.lines / 2), -- Height of the floating window
     default_mappings = false, -- Bind default mappings
     border = defaults.border,
     post_open_hook = function()
       vim.api.nvim_win_set_config(0, {
-        width = math.floor(vim.o.columns / 3), -- Width of the floating window
-        height = math.floor(vim.o.lines / 4), -- Height of the floating window
+        width = math.floor(vim.o.columns / 2), -- Width of the floating window
+        height = math.floor(vim.o.lines / 2), -- Height of the floating window
       })
     end,
   },
