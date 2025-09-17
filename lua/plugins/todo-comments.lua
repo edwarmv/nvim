63a78eff25bfb8ce4176a930141a93147bb3cf5e
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-09-17 10:51:27 -0400

    add trouble

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,9 +1,10 @@
 return {
   "folke/todo-comments.nvim",
   event = "VimEnter",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = { "nvim-lua/plenary.nvim", "folke/trouble.nvim" },
   opts = {},
   keys = {
+    { "<leader>xt", "<cmd>TodoTrouble<cr>" },
     {
       "]t",
       function()
