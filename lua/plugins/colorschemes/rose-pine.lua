51d342500fc4467629ad3a1ab62ed57579c3cbde
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-09-18 22:54:53 -0400

    update rose-pine

--- lua/plugins/colorschemes/rose-pine.lua
+++ lua/plugins/colorschemes/rose-pine.lua
@@ -8,13 +8,7 @@ return {
     highlight_groups = {
       ExtraWhitespace = { fg = "love", bg = "love", blend = 20 },
       ["@markup.link.label"] = {},
-      LuasnipInsertNodePassive = { bg = "gold", blend = 20 },
-      LuasnipChoiceNodePassive = { bg = "gold", blend = 20 },
-      CmpKitDeprecated = { link = "CmpItemAbbrDeprecated" },
-      CmpKitCompletionItemLabel = { link = "CmpItemAbbr" },
-      CmpKitCompletionItemDescription = { link = "CmpItemMenu" },
-      CmpKitCompletionItemMatch = { link = "CmpItemAbbrMatch" },
-      CmpKitCompletionItemExtra = { link = "CmpItemMenu" },
+      BlinkCmpDocBorder = { fg = "highlight_high", bg = "highlight_low" },
     },
   },
 }
