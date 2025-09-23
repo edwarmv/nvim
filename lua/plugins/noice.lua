35d4d2bb1054003a6238823cccf3e38c5609b393
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-09-23 08:56:52 -0400

    add noice

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,28 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    messages = {
+      view_search = false,
+    },
+    lsp = {
+      progress = {
+        enabled = false,
+      },
+      hover = {
+        enabled = false,
+      },
+      signature = {
+        enabled = false,
+      },
+    },
+    presets = {
+      command_palette = true,
+      long_message_to_split = true,
+      bottom_search = true,
+    },
+  },
+}
