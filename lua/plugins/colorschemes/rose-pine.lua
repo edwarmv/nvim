c512136d3b494ebc014ee7516dcfac516af485d6
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-09-23 08:54:10 -0400

    update rose-pine

--- lua/plugins/colorschemes/rose-pine.lua
+++ lua/plugins/colorschemes/rose-pine.lua
@@ -2,13 +2,17 @@ return {
   "rose-pine/neovim",
   enabled = true,
   lazy = false,
+  priority = 1000,
   name = "rose-pine",
   opts = {
     dim_inactive_windows = true,
     highlight_groups = {
       ExtraWhitespace = { fg = "love", bg = "love", blend = 20 },
       ["@markup.link.label"] = {},
-      BlinkCmpDocBorder = { fg = "highlight_high", bg = "highlight_low" },
+      LuasnipInsertNodePassive = { bg = "gold", blend = 20 },
+      LuasnipChoiceNodePassive = { bg = "gold", blend = 20 },
+      BlinkCmpDocBorder = { fg = "highlight_high", bg = "highlight_lok" },
+      BufferLineTabSelected = { fg = "text", bg = "base" },
     },
   },
 }
