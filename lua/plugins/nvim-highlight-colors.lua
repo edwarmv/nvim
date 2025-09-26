f7a8b8750009c719faf98208a28ea569986afbde
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-09-25 21:19:55 -0400

    update nvim-highlight-colors

--- lua/plugins/nvim-highlight-colors.lua
+++ lua/plugins/nvim-highlight-colors.lua
@@ -4,5 +4,9 @@ return {
     render = "virtual",
     virtual_symbol = "",
     enable_tailwind = true,
+    exclude_filetypes = {
+      "blink-cmp-menu",
+      -- "blink-cmp-documentation",
+    },
   },
 }
