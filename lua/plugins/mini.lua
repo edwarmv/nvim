4761d60b3285c0e54b09704dc3d7146c5eb5ac74
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-10-02 23:53:37 -0400

    replace luasnip with mini.snippets

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "echasnovski/mini.icons",
+    "nvim-mini/mini.icons",
     opts = {},
     lazy = true,
     specs = {
@@ -14,7 +14,7 @@ return {
     end,
   },
   {
-    "echasnovski/mini.diff",
+    "nvim-mini/mini.diff",
     event = "VeryLazy",
     opts = {
       view = { style = "number" },
@@ -39,7 +39,7 @@ return {
     },
   },
   {
-    "echasnovski/mini.indentscope",
+    "nvim-mini/mini.indentscope",
     init = function()
       vim.api.nvim_create_autocmd({ "BufAdd", "BufEnter" }, {
         desc = "Disable 'mini.indentscope' when buftype is ''",
@@ -52,5 +52,52 @@ return {
       })
     end,
     opts = { symbol = "▏" },
+  },
+  {
+    "nvim-mini/mini.snippets",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+    },
+    config = function()
+      local gen_loader = require("mini.snippets").gen_loader
+
+      local javascript = "**/javascript.json"
+      local react = "**/react.json"
+      local lang_patterns = {
+        typescript = { javascript },
+        astro = { javascript },
+        tsx = { react, javascript },
+      }
+      require("mini.snippets").setup({
+        snippets = {
+          -- -- Load custom file with global snippets first (adjust for Windows)
+          -- gen_loader.from_file('~/.config/nvim/snippets/global.json'),
+
+          -- Load snippets based on current language by reading files from
+          -- "snippets/" subdirectories from 'runtimepath' directories.
+          gen_loader.from_lang({
+            lang_patterns = lang_patterns,
+          }),
+        },
+        mappings = {
+          expand = "<C-j>",
+          jump_next = "",
+          jump_prev = "",
+          stop = "",
+        },
+      })
+      -- MiniSnippets.start_lsp_server()
+      local make_stop = function()
+        local au_opts = { pattern = "*:n", once = true }
+        au_opts.callback = function()
+          while MiniSnippets.session.get() do
+            MiniSnippets.session.stop()
+          end
+        end
+        vim.api.nvim_create_autocmd("ModeChanged", au_opts)
+      end
+      local opts = { pattern = "MiniSnippetsSessionStart", callback = make_stop }
+      vim.api.nvim_create_autocmd("User", opts)
+    end,
   },
 }
