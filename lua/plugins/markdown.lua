32b0be9edd9770cf42f3eced0508eb581c7b8d2d
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-10-02 23:52:38 -0400

    remove render-markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,27 +1,5 @@
 return {
   {
-    "MeanderingProgrammer/render-markdown.nvim",
-    opts = {
-      completion = {
-        blink = { enabled = true },
-      },
-      file_types = { "markdown", "Avante", "codecompanion" },
-      overrides = {
-        buftype = {
-          nofile = {
-            code = {
-              style = "normal",
-            },
-          },
-        },
-      },
-    },
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "echasnovski/mini.icons",
-    },
-  },
-  {
     "iamcco/markdown-preview.nvim",
     build = function()
       vim.fn["mkdp#util#install"]()
