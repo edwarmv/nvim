b4e61f4ff82468a20fc62b1c1de25b97733684b8
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-10-14 10:18:31 -0400

    remove undotree

--- lua/plugins/undotree.lua
+++
@@ -1,8 +0,0 @@
-vim.g.undotree_CustomDiffpanelCmd = "botright 10 new"
-return {
-  "mbbill/undotree",
-  enabled = true,
-  keys = {
-    { "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "Undotree - Toggle" },
-  },
-}
