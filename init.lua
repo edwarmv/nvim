611f3cfd22d6d25c1d06a24ea5de5a78fe9c9e7f
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-10-14 10:24:25 -0400

    update config

--- init.lua
+++ init.lua
@@ -1,108 +1,136 @@
-vim.deprecate = function() end
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-vim.o.winborder = "none"
-vim.opt.sessionoptions:remove({ "blank", "folds", "help" })
-vim.o.completeopt = "menu,menuone,noselect,fuzzy,nosort"
-vim.o.confirm = true -- Confirm to save changes before exiting modified buffer
-vim.o.cursorline = true -- Enable highlighting of the current line
-vim.o.cursorcolumn = true -- Enable highlighting of the current line
-vim.o.expandtab = true -- Use spaces instead of tabs
-vim.o.formatoptions = "jcroqlnt" -- tcqj
-vim.o.grepformat = "%f:%l:%c:%m"
-vim.o.grepprg = "rg --vimgrep"
-vim.o.ignorecase = true -- Ignore case
-vim.o.inccommand = "nosplit" -- preview incremental substitute
-vim.o.laststatus = 3 -- global statusline
-vim.o.cmdheight = 0
-vim.o.list = true -- Show some invisible characters (tabs...
-vim.o.number = true -- Print line number
-vim.o.pumblend = 0 -- Popup blend
-vim.o.pumheight = 6 -- Maximum number of entries in a popup
-vim.o.relativenumber = false -- Relative line numbers
-vim.o.shiftround = true -- Round indent
-vim.opt.shortmess:append({ W = true, I = true, c = true, C = true })
-vim.o.showmode = false -- Dont show mode since we have a statusline
-vim.o.signcolumn = "yes:1" -- Always show the signcolumn, otherwise it would shift the text each time
-vim.o.numberwidth = 1
-vim.o.smartcase = true -- Don't ignore case with capitals
-vim.opt.spelllang = { "en" }
-vim.opt.spelloptions:append("noplainbuffer")
-vim.o.splitbelow = true -- Put new windows below current
-vim.o.splitkeep = "screen"
-vim.o.splitright = true -- Put new windows right of current
-vim.o.tabstop = 4 -- Number of spaces tabs count for
-vim.o.shiftwidth = 2 -- Size of an indent
-vim.o.timeoutlen = 500
-vim.o.undofile = true
-vim.o.undolevels = 10000
-vim.o.updatetime = 300 -- Save swap file and trigger CursorHold
-vim.o.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
-vim.o.wildmode = "longest:full,full" -- Command-line completion mode
-vim.o.winminwidth = 5 -- Minimum window width
-vim.o.colorcolumn = "80"
-vim.o.breakindent = true
-vim.o.showbreak = "↪"
-vim.o.termguicolors = true
+-- Define config table to be able to pass data between scripts
+_G.Config = {}
 
--- Fix markdown indentation settings
-vim.g.markdown_recommended_style = 0
+-- Define custom autocommand group and helper to create an autocommand.
+-- Autocommands are Neovim's way to define actions that are executed on events
+-- (like creating a buffer, setting an option, etc.).
+--
+-- See also:
+-- - `:h autocommand`
+-- - `:h nvim_create_augroup()`
+-- - `:h nvim_create_autocmd()`
+local gr = vim.api.nvim_create_augroup("custom-config", {})
+_G.Config.new_autocmd = function(event, pattern, callback, desc)
+  local opts = { group = gr, pattern = pattern, callback = callback, desc = desc }
+  vim.api.nvim_create_autocmd(event, opts)
+end
 
--- Folding
-vim.opt.foldlevel = 99
-vim.opt.foldlevelstart = 99
-vim.opt.foldenable = true
-vim.opt.foldcolumn = "1"
-vim.opt.foldtext = ""
-vim.o.foldmethod = "expr"
-vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.opt.fillchars = {
-  foldopen = "▶",
-  foldclose = "▼",
-  diff = "╱",
-}
-vim.opt.listchars = {
-  -- multispace = "⋅",
-  trail = "⋅", -- ·
-  tab = "  ",
-  -- tab = "→⋅",
-  -- eol = "↵",
-}
+-- ┌──────────────────────────┐
+-- │ Built-in Neovim behavior │
+-- └──────────────────────────┘
+--
+-- This file defines Neovim's built-in behavior. The goal is to improve overall
+-- usability in a way that works best with MINI.
+--
+-- Here `vim.o.xxx = value` sets default value of option `xxx` to `value`.
+-- See `:h 'xxx'` (replace `xxx` with actual option name).
+--
+-- Option values can be customized on per buffer or window basis.
+-- See 'after/ftplugin/' for common example.
 
--- =========================================================
--- Mappings
--- =========================================================
-vim.keymap.set("i", "<c-b>", "<left>")
-vim.keymap.set("i", "<c-f>", "<right>")
-vim.keymap.set("i", "<c-n>", "<down>")
-vim.keymap.set("i", "<c-p>", "<up>")
-vim.keymap.set("c", "<c-a>", "<c-b>")
-vim.keymap.set("c", "<c-l>", "<c-f>")
-vim.keymap.set("c", "<c-b>", "<left>")
-vim.keymap.set("c", "<c-f>", "<right>")
-vim.keymap.set("n", "<c-w><c-l>", "<c-l><cmd>nohl<cr><cmd>lua Snacks.notifier.hide()<cr>")
-vim.keymap.set("n", "<leader>tn", "<cmd>$tabnew<cr>")
-vim.keymap.set("n", "<leader>tm", function()
-  vim.ui.input({ prompt = "Enter The Tab Index: " }, function(tab_index)
-    vim.cmd("tabmove" .. tab_index)
-  end)
-end, { desc = "Tab Move" })
-vim.keymap.set("n", "<leader>tc", "<cmd>tabclose<cr>")
-vim.keymap.set("n", "<leader>to", "<cmd>tabonly<cr>")
-vim.api.nvim_create_autocmd({ "TabLeave" }, {
-  command = "let g:lasttab = tabpagenr()",
-})
-vim.keymap.set("n", "<leader>tl", ":exe 'tabn '.g:lasttab<cr>", { silent = true, desc = "Last Tab" })
-vim.keymap.set("i", "<c-l>", "<c-f>")
-vim.keymap.set("i", "<s-tab>", "<c-d>")
-vim.keymap.set("n", "<c-w>z", ":resize | vertical resize<CR>")
+-- stylua: ignore start
+-- The next part (until `-- stylua: ignore end`) is aligned manually for easier
+-- reading. Consider preserving this or remove `-- stylua` lines to autoformat.
 
+-- General ====================================================================
+vim.g.mapleader                  = ' '            -- Use `<Space>` as <Leader> key
+vim.g.maplocalleader             = "\\"
+
+vim.o.mouse                      = 'a'            -- Enable mouse
+vim.o.mousescroll                = 'ver:25,hor:6' -- Customize mouse scroll
+vim.o.switchbuf                  = 'usetab'       -- Use already opened buffers when switching
+vim.o.undofile                   = true           -- Enable persistent undo
+vim.o.timeoutlen                 = 500
+vim.o.termguicolors              = true
+vim.g.markdown_recommended_style = 0              -- Fix markdown indentation settings
+
+vim.o.shada = "'100,<50,s10,:1000,/100,@100,h" -- Limit ShaDa file (for startup)
+
+-- Enable all filetype plugins and syntax (if not enabled, for better startup)
+vim.cmd('filetype plugin indent on')
+if vim.fn.exists('syntax_on') ~= 1 then vim.cmd('syntax enable') end
+
+-- UI =========================================================================
+vim.o.breakindent    = true       -- Indent wrapped lines to match line start
+vim.o.breakindentopt = 'list:-1'  -- Add padding for lists (if 'wrap' is set)
+vim.o.colorcolumn    = '+1'       -- Draw column on the right of maximum width
+vim.o.cursorline     = true       -- Enable current line highlighting
+vim.o.linebreak      = true       -- Wrap lines at 'breakat' (if 'wrap' is set)
+vim.o.list           = true       -- Show helpful text indicators
+vim.o.number         = true       -- Show line numbers
+vim.o.numberwidth    = 1
+vim.o.pumheight      = 10         -- Make popup menu smaller
+vim.o.ruler          = false      -- Don't show cursor coordinates
+vim.o.shortmess      = 'ICFOSWaco' -- Disable some built-in completion messages
+vim.o.showmode       = false      -- Don't show mode in command line
+vim.o.signcolumn     = 'yes:1'    -- Always show signcolumn (less flicker)
+vim.o.splitbelow     = true       -- Horizontal splits will be below
+vim.o.splitkeep      = 'screen'   -- Reduce scroll during window split
+vim.o.splitright     = true       -- Vertical splits will be to the right
+vim.o.winborder      = 'rounded'  -- Use border in floating windows
+vim.o.wrap           = false      -- Don't visually wrap lines (toggle with \w)
+vim.o.confirm        = true       -- Confirm to save changes before exiting modified buffer
+vim.o.showbreak      = "↪"
+
+vim.o.cursorlineopt  = 'screenline,number' -- Show cursor line per screen line
+
+-- Special UI symbols. More is set via 'mini.basics' later.
+vim.o.fillchars = 'eob: ,fold:╌,foldopen:▼,foldclose:▶,diff:╱,foldinner: ,foldsep: '
+vim.o.listchars = 'extends:…,nbsp:␣,precedes:…,tab:> ,trail:⋅'
+
+-- Folds (see `:h fold-commands`, `:h zM`, `:h zR`, `:h zA`, `:h zj`)
+vim.o.foldlevel   = 10       -- Fold nothing by default; set to 0 or 1 to fold
+vim.o.foldmethod  = 'indent' -- Fold based on indent level
+vim.o.foldnestmax = 10       -- Limit number of fold levels
+vim.o.foldtext    = ''       -- Show text under fold with its highlighting
+vim.o.foldcolumn  = "1"
+
+-- Editing ====================================================================
+vim.o.autoindent    = true    -- Use auto indent
+vim.o.expandtab     = true    -- Convert tabs to spaces
+vim.o.formatoptions = 'rqnl1j'-- Improve comment editing
+vim.o.ignorecase    = true    -- Ignore case during search
+vim.o.incsearch     = true    -- Show search matches while typing
+vim.o.infercase     = true    -- Infer case in built-in completion
+vim.o.shiftwidth    = 2       -- Use this number of spaces for indentation
+vim.o.smartcase     = true    -- Respect case if search pattern has upper case
+vim.o.smartindent   = true    -- Make indenting smart
+vim.o.spelloptions  = 'camel' -- Treat camelCase word parts as separate words
+vim.o.tabstop       = 4       -- Show tab as this number of spaces
+vim.o.virtualedit   = 'block' -- Allow going past end of line in blockwise mode
+
+vim.o.iskeyword = '@,48-57,_,192-255,-' -- Treat dash as `word` textobject part
+
+-- Pattern for a start of numbered list (used in `gw`). This reads as
+-- "Start of list item is: at least one special character (digit, -, +, *)
+-- possibly followed by punctuation (. or `)`) followed by at least one space".
+vim.o.formatlistpat = [[^\s*[0-9\-\+\*]\+[\.\)]*\s\+]]
+
+-- Built-in completion
+vim.o.complete    = '.,w,b,kspell'                  -- Use less sources
+vim.o.completeopt = 'menuone,noselect,fuzzy,nosort' -- Use custom behavior
+
+-- Autocommands ===============================================================
+
+-- Don't auto-wrap comments and don't insert comment leader after hitting 'o'.
+-- Do on `FileType` to always override these changes from filetype plugins.
+local f = function() vim.cmd('setlocal formatoptions-=c formatoptions-=o') end
+_G.Config.new_autocmd('FileType', nil, f, "Proper 'formatoptions'")
+
+-- There are other autocommands created by 'mini.basics'. See 'plugin/30_mini.lua'.
+
+-- Diagnostics ================================================================
+
+-- Neovim has built-in support for showing diagnostic messages. This configures
+-- a more conservative display while still being useful.
+-- See `:h vim.diagnostic` and `:h vim.diagnostic.config()`.
 local defaults = require("config.defaults")
 local icons = defaults.icons
-
-vim.diagnostic.config({
-  virtual_text = true,
+local diagnostic_opts = {
+  -- Show signs on top of any other sign, but only for warnings and errors
   signs = {
+    priority = 9999,
+    severity = { min = 'WARN', max = 'ERROR' },
     text = {
       [vim.diagnostic.severity.ERROR] = icons.diagnostics.error,
       [vim.diagnostic.severity.WARN] = icons.diagnostics.warn,
@@ -110,48 +138,28 @@ vim.diagnostic.config({
       [vim.diagnostic.severity.HINT] = icons.diagnostics.hint,
     },
   },
-})
 
-local function augroup(name)
-  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
-end
+  -- Show all diagnostics as underline (for their messages type `<Leader>ld`)
+  underline = { severity = { min = 'HINT', max = 'ERROR' } },
 
--- close some filetypes with <q>
-vim.api.nvim_create_autocmd("FileType", {
-  group = augroup("close_with_q"),
-  pattern = {
-    "PlenaryTestPopup",
-    "help",
-    "lspinfo",
-    "man",
-    "notify",
-    "qf",
-    "query",
-    "spectre_panel",
-    "startuptime",
-    "tsplayground",
-    "neotest-output",
-    "checkhealth",
-    "neotest-summary",
-    "neotest-output-panel",
-    "harpoon",
+  -- Show more details immediately for errors on the current line
+  virtual_lines = false,
+  virtual_text = {
+    current_line = true,
+    severity = { min = 'ERROR', max = 'ERROR' },
   },
-  callback = function(event)
-    vim.bo[event.buf].buflisted = false
-    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
-  end,
-})
 
--- wrap and check for spell in text filetypes
-vim.api.nvim_create_autocmd("FileType", {
-  group = augroup("wrap_spell"),
-  pattern = { "gitcommit", "NeogitCommitMessage", "markdown" },
-  callback = function()
-    vim.opt_local.wrap = true
-    vim.opt_local.spell = true
-  end,
-})
+  -- Don't update diagnostics when typing
+  update_in_insert = false,
+}
+
+-- Use `later()` to avoid sourcing `vim.diagnostic` on startup
+vim.diagnostic.config(diagnostic_opts)
+
+-- load ================================================================
 
 require("config")
 
 vim.cmd.colorscheme("rose-pine")
+
+-- stylua: ignore end
