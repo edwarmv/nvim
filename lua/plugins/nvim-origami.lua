55a797af0e81845f70726ae66a67af0c72c50105
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-10-14 18:04:22 -0400

    add nvim-origami

---
+++ lua/plugins/nvim-origami.lua
@@ -0,0 +1,12 @@
+return {
+  "chrisgrieser/nvim-origami",
+  event = "VeryLazy",
+  opts = {
+    autoFold = {
+      enabled = false,
+    },
+    foldKeymaps = {
+      setup = false,
+    },
+  },
+}
