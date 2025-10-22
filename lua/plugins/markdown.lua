b1c2940b5dbb770be33f7de220c064afcc9e0462
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-10-22 09:42:23 -0400

    add render-markdown.nvim

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,5 +1,35 @@
 return {
   {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      completion = {
+        blink = { enabled = true },
+      },
+      file_types = { "markdown", "Avante", "codecompanion" },
+      code = {
+        border = "thin",
+      },
+      win_options = {
+        conceallevel = {
+          rendered = 0,
+        },
+      },
+      overrides = {
+        buftype = {
+          nofile = {
+            code = {
+              style = "normal",
+            },
+          },
+        },
+      },
+    },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "echasnovski/mini.icons",
+    },
+  },
+  {
     "iamcco/markdown-preview.nvim",
     build = function()
       vim.fn["mkdp#util#install"]()
