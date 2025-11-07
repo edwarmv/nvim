0f578cd734170748c9e93454fb6e3e8cdd76a5bf
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-11-07 19:31:02 -0400

    remove treejs

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -76,14 +76,4 @@ return {
       },
     },
   },
-  {
-    "Wansmer/treesj",
-    keys = {
-      { "<leader>m", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
-    },
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    opts = {
-      use_default_keymaps = false,
-    },
-  },
 }
