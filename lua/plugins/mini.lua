ecc9ef54d25e9108e999c59da2134afdcff2d1cb
Edwar Martinez <edwar_kenedy@hotmail.com>
2025-11-07 19:30:37 -0400

    add mini.snippets disabled

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -38,4 +38,47 @@ return {
       },
     },
   },
+  {
+    "nvim-mini/mini.snippets",
+    enabled = false,
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+    },
+    config = function()
+      local gen_loader = require("mini.snippets").gen_loader
+
+      local javascript = "**/javascript.json"
+      local react = "**/react.json"
+      local lang_patterns = {
+        typescript = { javascript },
+        astro = { javascript },
+        tsx = { react, javascript },
+      }
+      require("mini.snippets").setup({
+        snippets = {
+          -- -- Load custom file with global snippets first (adjust for Windows)
+          -- gen_loader.from_file('~/.config/nvim/snippets/global.json'),
+
+          -- Load snippets based on current language by reading files from
+          -- "snippets/" subdirectories from 'runtimepath' directories.
+          gen_loader.from_lang({
+            lang_patterns = lang_patterns,
+          }),
+        },
+        mappings = {
+          jump_next = "",
+          jump_prev = "",
+          stop = "",
+        },
+        expand = {
+          insert = function(snippet, _)
+            -- require("ix").do_action(function(ctx)
+            --   ctx.completion.hide()
+            -- end)
+            vim.snippet.expand(snippet.body)
+          end,
+        },
+      })
+    end,
+  },
 }
